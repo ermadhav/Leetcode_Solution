a453176a987564b44ce9090a8765ec3aefe65736
ermadhav <madhavtiwari2024@gmail.com>
2025-01-02 15:33:01 +0530

    nothing much

--- 35.Search_Insert_Position.java
+++ 35.Search_Insert_Position.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0; i < nums.length; i++){
             // If target is less than or equal to current number, return the index
             if(nums[i] >= target){
-                return i;  // if 
+                return i;  
             } 
         }
         // If target is greater than all numbers, return the end index   
