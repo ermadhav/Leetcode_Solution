a7aee64e524659780798bf57b2c96b184130f3b0
ermadhav <madhavtiwari2024@gmail.com>
2025-01-02 20:48:31 +0530

    Added some comments in the code so that other can understand

--- 1572.Matrix_Diagonal_Sum.java
+++ 1572.Matrix_Diagonal_Sum.java
@@ -4,6 +4,7 @@ class Solution {
         int primarySum = 0;
         int secondarySum = 0;
 
+        // Iterate through each row
         for(int i =0; i < n; i++){
             primarySum += mat[i][i];
             secondarySum += mat[i][n-1-i];
@@ -11,8 +12,10 @@ class Solution {
 
         if(n % 2 != 0){
             int centerElement = mat[n/2][n/2];
-            primarySum -= centerElement; 
+            primarySum -= centerElement;  // Subtract the center element once as it's included in both diagonals
         }
+        
+        // Return the sum of both diagonals
         return primarySum + secondarySum;
     }
 }
