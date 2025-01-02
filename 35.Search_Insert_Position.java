e490a3b42894fb62d895ee667afcf99dc19ac803
ermadhav <madhavtiwari2024@gmail.com>
2025-01-02 15:30:48 +0530

    Adding some comments

--- 35.Search_Insert_Position.java
+++ 35.Search_Insert_Position.java
@@ -1,11 +1,10 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
 
-        // traversing
-
+        // Traversing Through the Loop
         for(int i=0; i < nums.length; i++){
             if(nums[i] >= target){
-                return i;
+                return i;  // if 
             } 
         }   
         return nums.length;
