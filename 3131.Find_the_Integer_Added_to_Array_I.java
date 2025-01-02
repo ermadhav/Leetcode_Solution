04268c431743309cc86135b120fb583889b43a4d
ermadhav <madhavtiwari2024@gmail.com>
2025-01-02 19:40:32 +0530

    final commit of these file

--- 3131.Find_the_Integer_Added_to_Array_I.java
+++ 3131.Find_the_Integer_Added_to_Array_I.java
@@ -3,10 +3,10 @@ import java.util.Arrays;
 class Solution {
     public int addedInteger(int[] nums1, int[] nums2) {
         int x=0;
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        Arrays.sort(nums1); // Sorting nums1 
+        Arrays.sort(nums2); // Sorting nums2
         for(int i=0; i < nums1.length; i++){
-             x = nums2[i] - nums1[i];
+             x = nums2[i] - nums1[i];  // Finding the Difference(x) 
         }
         return x;
     }
