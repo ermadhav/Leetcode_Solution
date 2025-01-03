552b1f33d459b343fc98aad2e400ee33f6a29d70
ermadhav <madhavtiwari2024@gmail.com>
2025-01-03 14:47:00 +0530

    Added comments in the code

--- 2114.Maximum_Number_of_Words_Found_in_Sentences.java
+++ 2114.Maximum_Number_of_Words_Found_in_Sentences.java
@@ -3,6 +3,7 @@ class Solution {
         int maxCount = 0;
         for(int i = 0; i < sentences.length ; i++){
             int count = 1;
+            // Count the spaces in the sentence 
             for(int j = 0; j< sentences[i].length(); j++){
                 if(sentences[i].charAt(j) ==' '){
                     count++;
