c499d0d068888704174b3032423972127b958b96
ermadhav <madhavtiwari2024@gmail.com>
2025-01-03 15:43:26 +0530

    added comment in the solution

--- 1528.Shuffle_String.java
+++ 1528.Shuffle_String.java
@@ -1,8 +1,9 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
 
-        char[] shuffledString = new char[s.length()];
+        char[] shuffledString = new char[s.length()]; // Created a new array to store the suffled array
 
+        // This for loop place the character in correct place
         for (int i=0; i < s.length(); i++){
             shuffledString[indices[i]] = s.charAt(i);
         }
