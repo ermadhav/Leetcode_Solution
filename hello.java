1d51c62dc42870d2cf133ea56b9d857b132748ed
ermadhav <madhavtiwari2024@gmail.com>
2025-01-04 21:33:42 +0530

    solved the Qs

--- hello.java
+++ hello.java
@@ -1,5 +1,17 @@
 class Solution {
-    public List<String> summaryRanges(int[] nums) {
+    public int findMaxConsecutiveOnes(int[] nums) {
         
+        int maxStreak = 0;
+        int currentStreak = 0;
+
+        for(int num: nums){
+            if(num == 1){
+                currentStreak++;
+                maxStreak = Math.max(maxStreak, currentStreak);
+            }else{
+                currentStreak = 0;
+            }
+        }
+        return maxStreak;
     }
 }
