3d91f7626c738249c20dcf0cd7cf7915b490989e
ermadhav <madhavtiwari2024@gmail.com>
2025-01-05 18:13:43 +0530

    added some comments in it

--- 2011.Final_Value_of_Variable_After_Performing_Operations.java
+++ 2011.Final_Value_of_Variable_After_Performing_Operations.java
@@ -1,10 +1,17 @@
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
         int X = 0;
+
+        // Traversing all element 
         for(int i = 0; i < operations.length; i++ ){
+
+            // Check if the operation is an increment operation (++X or X++)
             if(operations[i].equals("++X") || operations[i].equals("X++")) {
                 X++ ;
-            } else if (operations[i].equals("--X") || operations[i].equals("X--")){
+            } 
+            
+            // Check if the operation is a decrement operation (--X or X--)            
+            else if (operations[i].equals("--X") || operations[i].equals("X--")){
                 X--;
             }
         }
