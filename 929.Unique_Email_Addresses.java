2f11f3a386ff2ecf1d8cf1f244ec4c3bd3d3ed1a
ermadhav <madhavtiwari2024@gmail.com>
2025-01-05 19:10:20 +0530

    Added comment in the solution

--- 929.Unique_Email_Addresses.java
+++ 929.Unique_Email_Addresses.java
@@ -2,10 +2,16 @@
 
 class Solution {
     public int numUniqueEmails(String[] emails) {
+
+        //Created a hash map to store a unique mail
         HashSet<String> uniqueEmails = new HashSet<>();
         
         for (String email : emails) {
+            // Split the array into local and domain 
             String[] parts = email.split("@");
+
+            // Remove everything after the first '+' 
+            // and remove all '.' from the local part
             String local = parts[0].split("\\+")[0].replace(".", "");
             uniqueEmails.add(local + "@" + parts[1]);
         }
