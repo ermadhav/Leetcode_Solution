ad02214ad0eae1d54d962b44ddbdc204c23aed72
ermadhav <madhavtiwari2024@gmail.com>
2025-01-06 10:44:21 +0530

    Added some Comment in it

--- 1816.Truncate_Sentence.java
+++ 1816.Truncate_Sentence.java
@@ -1,13 +1,18 @@
 class Solution {
     public String truncateSentence(String s, int k) {
+        // Convert the input Sring into Char Array
         char[] newStr = s.toCharArray();
+
+        // Split the input string `s` into an array of word
         String[] words = s.split(" ");
 
         StringBuilder arr = new StringBuilder();
 
         for (int i=0; i<k && i<words.length; i++){
-            arr.append(words[i]).append(" ");
+            arr.append(words[i]).append(" "); // Append the word followed by a space
         }
+
+        // Trim any trailing spaces and return
         return arr.toString().trim();
     }
 }
