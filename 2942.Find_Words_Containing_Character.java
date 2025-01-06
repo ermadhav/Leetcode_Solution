aa3655a7c77ff3a0b4852e08266398b54004f7b3
ermadhav <madhavtiwari2024@gmail.com>
2025-01-06 14:10:02 +0530

    Solved the Qs

--- 2942.Find_Words_Containing_Character.java
+++ 2942.Find_Words_Containing_Character.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<Integer> findWordsContaining(String[] words, char x) {
         List<Integer> indices = new ArrayList<>();
         for(int i=0; i<words.length; i++){
 
+            
+            // Check if the word contains the character x
             if(words[i].indexOf(x) != -1)  
             indices.add(i);
 
