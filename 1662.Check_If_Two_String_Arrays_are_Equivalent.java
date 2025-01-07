87922e93ec79b561db8aad85a9d8fefe011a087c
ermadhav <madhavtiwari2024@gmail.com>
2025-01-07 13:50:03 +0530

    Added the comments in it

--- 1662.Check_If_Two_String_Arrays_are_Equivalent.java
+++ 1662.Check_If_Two_String_Arrays_are_Equivalent.java
@@ -1,8 +1,12 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+
+        // Combining All Strings in word1 and word2
+
         String newWord1 = String.join("", word1);
         String newWord2 = String.join("", word2);
 
+        // Comparing both combined Strings and returning the true if they are equal
         if(newWord1.compareTo(newWord2) == 0){
             return true;
         }
