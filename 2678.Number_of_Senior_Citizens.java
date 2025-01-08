2b8f49c02fe7f1b3d4f5955a4ea0b5abfc67495f
ermadhav <madhavtiwari2024@gmail.com>
2025-01-08 15:44:45 +0530

    Adding comments in the Qs

--- 2678.Number_of_Senior_Citizens.java
+++ 2678.Number_of_Senior_Citizens.java
@@ -1,6 +1,8 @@
 class Solution {
     public int countSeniors(String[] details) {
         int count =0;
+
+        //Iterating Through details using for loop
         for(int i=0; i<details.length; i++){
         String newDetails = details[i].substring(11,13);
         int numeric = Integer.parseInt(newDetails);
