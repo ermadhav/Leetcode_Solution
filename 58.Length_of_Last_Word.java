38a1bad476c3c1ad880e19ad75cd0ee224a31a3b
ermadhav <madhavtiwari2024@gmail.com>
2025-01-08 14:58:33 +0530

    Adding some comments

--- 58.Length_of_Last_Word.java
+++ 58.Length_of_Last_Word.java
@@ -3,6 +3,8 @@ class Solution {
 
         //Spliting the String
         String[] newString = s.split(" ");
+
+        // String the Length of newString in l
         int l = newString.length;
         String a = newString[l-1];
         int ans = a.length();
