501010795f73aad518a75eb650a6e405775f1edb
ermadhav <madhavtiwari2024@gmail.com>
2025-01-09 13:35:04 +0530

    Added the comment in the solution

--- 1967.Number_of_Strings_That_Appear_as_Substrings_in_Word.java
+++ 1967.Number_of_Strings_That_Appear_as_Substrings_in_Word.java
@@ -1,6 +1,8 @@
 class Solution {
     public int numOfStrings(String[] patterns, String word) {
         int count =0;
+
+        // Iterating over each string in the patterns array.
        for(int i=0; i<patterns.length; i++){
         if(word.contains(patterns[i])){
             count++;
