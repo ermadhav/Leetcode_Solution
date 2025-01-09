0618091ef608a42b97d3ec4d99794c15cc25707b
ermadhav <madhavtiwari2024@gmail.com>
2025-01-10 02:26:41 +0530

    nothing much

--- 3110.Score_of_a_String.java
+++ 3110.Score_of_a_String.java
@@ -1,12 +1,13 @@
 class Solution {
     public int scoreOfString(String s) {
-        int sum =0;
+        int sum = 0;
 
-        //Interating through the String
-        for(int i=1; i<s.length(); i++){
+        // Interating through the String
+        for (int i = 1; i < s.length(); i++) {
 
-        // Adding the absolute difference between the current character and the previous character to the sum.
-        sum += Math.abs(s.charAt(i-1)-s.charAt(i));
+            // Adding the absolute difference between the current character and the previous
+            // character to the sum.
+            sum += Math.abs(s.charAt(i - 1) - s.charAt(i));
         }
         return sum;
     }
