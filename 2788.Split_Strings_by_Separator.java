5b5a674508f823c8ba0236192f5ad9e0e3f23864
ermadhav <madhavtiwari2024@gmail.com>
2025-01-09 14:23:54 +0530

    Added some comments in it

--- 2788.Split_Strings_by_Separator.java
+++ 2788.Split_Strings_by_Separator.java
@@ -1,26 +1,29 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern; 
+import java.util.regex.Pattern;
+
 class Solution {
     public List<String> splitWordsBySeparator(List<String> words, char separator) {
-        
+
         List<String> result = new ArrayList<>();
 
         // Convert separator char to string
         String separatorStr = String.valueOf(separator);
 
-        for(int i=0; i<words.size(); i++){
+        for (int i = 0; i < words.size(); i++) {
 
+            // Split the word by the separator
             String[] newWords = words.get(i).split(Pattern.quote(separatorStr));
 
-            for(int j=0; j<newWords.length; j++){
-                if(newWords[j].length() >0){
+            // Only add non-empty parts to the result
+            for (int j = 0; j < newWords.length; j++) {
+                if (newWords[j].length() > 0) {
                     result.add(newWords[j]);
                 }
             }
 
         }
-        
+
         return result;
     }
 }
