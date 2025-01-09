4e20520b2846cfae02d76e16949de9ee17b32859
ermadhav <madhavtiwari2024@gmail.com>
2025-01-10 02:26:09 +0530

    nothing much

--- 3110.Score_of_a_String.java
+++ 3110.Score_of_a_String.java
@@ -4,6 +4,8 @@ class Solution {
 
         //Interating through the String
         for(int i=1; i<s.length(); i++){
+
+            // Adding the absolute difference between the current character and the previous character to the sum.
             sum += Math.abs(s.charAt(i-1)-s.charAt(i));
 
 
