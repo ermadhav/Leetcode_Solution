1d46073cdc203dcea6e827e3578dc024dcd0a11a
ermadhav <madhavtiwari2024@gmail.com>
2025-01-09 14:20:41 +0530

    Solved the Qs

--- 2788.Split_Strings_by_Separator.java
+++ 2788.Split_Strings_by_Separator.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern; 
 class Solution {
     public List<String> splitWordsBySeparator(List<String> words, char separator) {
@@ -6,9 +8,7 @@ class Solution {
         String separatorStr = String.valueOf(separator);
 
         for(int i=0; i<words.size(); i++){
-            // String[] newWords = words.split(separator);
 
-            // String[] newWords = words.get(i).split(Pattern.qoute(separatorStr));
             String[] newWords = words.get(i).split(Pattern.quote(separatorStr));
 
             for(int j=0; j<newWords.length; j++){
