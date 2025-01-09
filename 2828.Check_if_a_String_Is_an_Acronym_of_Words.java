15270f2c7ce0ef4a5053a280c2344c09b3d5c606
ermadhav <madhavtiwari2024@gmail.com>
2025-01-10 02:58:49 +0530

    nothing much

--- 2828.Check_if_a_String_Is_an_Acronym_of_Words.java
+++ 2828.Check_if_a_String_Is_an_Acronym_of_Words.java
@@ -2,7 +2,11 @@ import java.util.List;
 
 class Solution {
     public boolean isAcronym(List<String> words, String s) {
+        
+         // Initialize an empty string
         String sum = "";
+
+
         for(int i=0; i<words.size(); i++){
                 sum += words.get(i).charAt(0);
         }
