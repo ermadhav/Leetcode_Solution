d84b665e1d171f7a52e2dbfe5a239fc9bdc7b43b
ermadhav <madhavtiwari2024@gmail.com>
2025-01-10 02:24:52 +0530

    nothing much

--- 3110.Score_of_a_String.java
+++ 3110.Score_of_a_String.java
@@ -1,6 +1,8 @@
 class Solution {
     public int scoreOfString(String s) {
         int sum =0;
+
+        //Interating through the String
         for(int i=1; i<s.length(); i++){
             sum += Math.abs(s.charAt(i-1)-s.charAt(i));
 
