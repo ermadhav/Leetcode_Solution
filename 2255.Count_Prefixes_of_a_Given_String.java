15da9c284bf6036066e12bc9533ec408e5e65581
ermadhav <madhavtiwari2024@gmail.com>
2025-01-12 13:38:20 +0530

    Added comments in it

--- 2255.Count_Prefixes_of_a_Given_String.java
+++ 2255.Count_Prefixes_of_a_Given_String.java
@@ -1,6 +1,8 @@
 class Solution {
     public int countPrefixes(String[] words, String s) {
         int count =0;
+
+        // Iterate through each word in the array 'words'
         for(int i=0; i<words.length; i++){
             if(s.startsWith(words[i])){
                 count++;
