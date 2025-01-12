5d5096b7ea5d2c67c3a78e8806b2f2a4db28a3f7
ermadhav <madhavtiwari2024@gmail.com>
2025-01-12 13:38:39 +0530

    Added comments in it

--- 2255.Count_Prefixes_of_a_Given_String.java
+++ 2255.Count_Prefixes_of_a_Given_String.java
@@ -4,6 +4,8 @@ class Solution {
 
         // Iterate through each word in the array 'words'
         for(int i=0; i<words.length; i++){
+
+             // Check if the string 's' starts with the current word
             if(s.startsWith(words[i])){
                 count++;
             }
