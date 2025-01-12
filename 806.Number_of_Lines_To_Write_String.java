5ad250e72f5aafcda1e640a09cd991d1ef99ce23
ermadhav <madhavtiwari2024@gmail.com>
2025-01-12 14:03:57 +0530

    Added comments in it

--- 806.Number_of_Lines_To_Write_String.java
+++ 806.Number_of_Lines_To_Write_String.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] numberOfLines(int[] widths, String s) {
-        int sum=0, row =1;
+        int sum=0, row =1; // Start with 1 row, as there will always be at least one line
         for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
             sum += widths[ch -'a'];
