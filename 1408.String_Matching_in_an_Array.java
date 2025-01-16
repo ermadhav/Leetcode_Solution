4a3abc61df3060b6a73146c2954a37b2f71a767d
ermadhav <madhavtiwari2024@gmail.com>
2025-01-16 13:51:10 +0530

    Added comments to the soln

--- 1408.String_Matching_in_an_Array.java
+++ 1408.String_Matching_in_an_Array.java
@@ -4,6 +4,8 @@ import java.util.List;
 class Solution {
     public List<String> stringMatching(String[] words) {
         List<String> result = new ArrayList<>();
+
+        // Iterate Through each words
         for(int i=0; i<words.length; i++){
             for(int j=0; j<words.length; j++){
                 if(i!=j && words[j].contains(words[i])){
