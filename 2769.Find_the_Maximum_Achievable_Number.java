b4c34501fdec0b07db3e97bb6942896b04d6b384
ermadhav <madhavtiwari2024@gmail.com>
2025-01-17 13:59:09 +0530

    Added the comments in it

--- 2769.Find_the_Maximum_Achievable_Number.java
+++ 2769.Find_the_Maximum_Achievable_Number.java
@@ -1,6 +1,7 @@
 class Solution {
     public int theMaximumAchievableX(int num, int t) {
-        // The maximum achievable x can be found by increasing x t times 
+        // The maximum achievable x can be found by increasing x t times
+         
         // and simultaneously decreasing num t times. This gives num + 2 * t.
         return num + 2*t;
     }
