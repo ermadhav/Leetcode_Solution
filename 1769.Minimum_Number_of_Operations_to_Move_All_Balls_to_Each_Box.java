23a40438f90e4a4efbb4415f2629ce35c1c6c167
ermadhav <madhavtiwari2024@gmail.com>
2025-01-19 13:15:23 +0530

    Solved the Qs

---
+++ 1769.Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] minOperations(String boxes) {
+        int n = boxes.length();
+        int[] answer = new int[n];
+        
+        // First pass: Calculate operations moving from left to right
+        int count = 0; // Number of balls encountered so far
+        int operations = 0; // Operations needed to move balls to current position
+        for (int i = 0; i < n; i++) {
+            answer[i] += operations;
+            if (boxes.charAt(i) == '1') {
+                count++;
+            }
+            operations += count; // Increment operations based on balls encountered so far
+        }
+        
+        // Second pass: Calculate operations moving from right to left
+        count = 0;
+        operations = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] += operations;
+            if (boxes.charAt(i) == '1') {
+                count++;
+            }
+            operations += count; // Increment operations based on balls encountered so far
+        }
+        
+        return answer;
+    }
+}
