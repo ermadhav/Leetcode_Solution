0c4d5732757d26def84e95976e6907c2797ce8e4
ermadhav <madhavtiwari2024@gmail.com>
2025-01-20 14:02:17 +0530

    Solved the new Qs

---
+++ 804.Unique_Morse_Code_words.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        // Morse code representations for each letter in the English alphabet
+        String[] morseCode = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
+        // A set to store unique transformations
+        Set<String> transformations = new HashSet<>();
+
+        // Iterate through each word in the array
+        for (String word : words) {
+            StringBuilder transformation = new StringBuilder();
+
+            // Convert each character in the word to its Morse code equivalent
+            for (char c : word.toCharArray()) {
+                transformation.append(morseCode[c - 'a']);
+            }
+
+            // Add the transformation to the set
+            transformations.add(transformation.toString());
+        }
+
+        // The size of the set represents the number of unique transformations
+        return transformations.size();
+    }
+}
