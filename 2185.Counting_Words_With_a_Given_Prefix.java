3232f6b208c7a9ff0bf8c135f982c5d1a0c41b08
ermadhav <madhavtiwari2024@gmail.com>
2025-01-25 14:11:27 +0530

    Solved the new qs

--- 2185.Counting_Words_With_a_Given_Prefix.java
+++ 2185.Counting_Words_With_a_Given_Prefix.java
@@ -3,6 +3,7 @@ class Solution {
         int count = 0;// Initialize counter to track words with the prefix
         // Iterate through each word in the array
         for (String word : words) {
+            // Check if the word starts with the specified prefix
             if (word.startsWith(pref)) {
                 count++;
             }
