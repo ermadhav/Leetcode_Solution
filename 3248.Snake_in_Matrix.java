53f71a5810c3b66787e7ace15ed3eb74fdca4bec
ermadhav <madhavtiwari2024@gmail.com>
2025-01-27 17:48:51 +0530

    Solveda new Qs

---
+++ 3248.Snake_in_Matrix.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+class Solution {
+    public int finalPositionOfSnake(int n, List<String> commands) {
+        // Initial position (row, col)
+        int row = 0, col = 0;
+        
+        // Process each command
+        for (String command : commands) {
+            switch (command) {
+                case "UP":
+                    row -= 1;
+                    break;
+                case "DOWN":
+                    row += 1;
+                    break;
+                case "LEFT":
+                    col -= 1;
+                    break;
+                case "RIGHT":
+                    col += 1;
+                    break;
+            }
+        }
+        
+        // Convert the final position (row, col) to a single integer
+        return row * n + col;
+    }
+}
