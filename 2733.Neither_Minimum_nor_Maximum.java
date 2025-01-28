86906b06a3a85a76e65740b4e3acd6197b57120a
ermadhav <madhavtiwari2024@gmail.com>
2025-01-28 14:25:01 +0530

    Solved another Qs

---
+++ 2733.Neither_Minimum_nor_Maximum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findNonMinOrMax(int[] nums) {
+        for(int i=0; i<nums.length-1; i++){
+            for(int j=0; j<nums.length-i-1; j++){
+                if(nums[j] > nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }if(nums.length == 2 || nums.length == 1){
+            return -1;
+        }else if(nums.length % 2 == 0){
+            return nums[nums.length/2];
+        }else{
+            int num = Math.round(nums.length/2);
+            return nums[num];
+        }
+    }
+}
