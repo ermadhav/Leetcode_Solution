4b38a88aa3f37d4752e3c73b71e4fda28b0211ee
ermadhav <madhavtiwari2024@gmail.com>
2025-01-28 15:02:51 +0530

    Solved another Qs

--- 1913.Maximum_Product_Difference_Between_Two_Pairs.java
+++ 1913.Maximum_Product_Difference_Between_Two_Pairs.java
@@ -1,20 +1,26 @@
 class Solution {
     public int maxProductDifference(int[] nums) {
-        // int[] sortedArray = Array.sort(nums)
-
-        for(int i=0; i<nums.length-1; i++){
-            int smallest =i;
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[smallest] > nums[j]){
+        // Sort the array using selection sort
+        for (int i = 0; i < nums.length - 1; i++) {
+            int smallest = i; // Assume the current index holds the smallest value
+            for (int j = i + 1; j < nums.length; j++) {
+                // Find the smallest value in the remaining array
+                if (nums[smallest] > nums[j]) {
                     smallest = j;
                 }
             }
+            // Swap the smallest value found with the value at the current index
             int temp = nums[smallest];
             nums[smallest] = nums[i];
             nums[i] = temp;
         }
-        int l= nums.length;
-        int ans = Math.abs((nums[0]*nums[1])-(nums[l-1]*nums[l-2]));
+
+        // After sorting, the smallest values are at the start and the largest at the end
+        int l = nums.length;
+        
+        // Calculate the maximum product difference
+        // (product of two largest numbers - product of two smallest numbers)
+        int ans = Math.abs((nums[0] * nums[1]) - (nums[l - 1] * nums[l - 2]));
         return ans;
     }
 }
