c47362b1d3ebc45c7b06d11b445050ede828f98f
ermadhav <madhavtiwari2024@gmail.com>
2025-01-29 13:10:37 +0530

    Added comments in it

--- 1051.Height_Checker.java
+++ 1051.Height_Checker.java
@@ -1,12 +1,16 @@
+import java.util.Arrays;
+
 class Solution {
     public int heightChecker(int[] heights) {
         int count = 0;
         int[] expected = new int[heights.length];
 
+        // Copy heights array to expected before sorting
         for (int i = 0; i < heights.length; i++) {
             expected[i] = heights[i];
         }
 
+        // Sort the expected array (Bubble Sort from your original code)
         for (int i = 0; i < expected.length; i++) {
             for (int j = 0; j < expected.length - i - 1; j++) {
                 if (expected[j] > expected[j + 1]) {
@@ -17,6 +21,7 @@ class Solution {
             }
         }
 
+        // Count mismatches
         for (int i = 0; i < heights.length; i++) {
             if (heights[i] != expected[i]) {
                 count++;
@@ -26,4 +31,3 @@ class Solution {
         return count;
     }
 }
-
