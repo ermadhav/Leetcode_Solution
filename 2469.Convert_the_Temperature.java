7acee8616050ffd7fb9a3d4b8245561f7be88d4b
ermadhav <madhavtiwari2024@gmail.com>
2025-01-29 12:24:47 +0530

    Added comment in it

--- 2469.Convert_the_Temperature.java
+++ 2469.Convert_the_Temperature.java
@@ -4,7 +4,7 @@ class Solution {
         double Kelvin = celsius + 273.15;
         // Convert Celsius to Fahrenheit
         double Fahrenheit = celsius * 1.80 + 32.00;
-
+         // Store the results in an array 
         double[] ans = {Kelvin, Fahrenheit};
         return ans;
     }
