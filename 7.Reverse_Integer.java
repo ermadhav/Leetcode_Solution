eb79151ba88c3f30411ee6b1b999a6d606b6949b
ermadhav <madhavtiwari2024@gmail.com>
2025-02-01 11:37:00 +0530

    Solved

---
+++ 7.Reverse_Integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+        StringBuilder sb = new StringBuilder(String.valueOf(Math.abs(x)));
+        sb.reverse();
+        int lastElement = sb.charAt(sb.length() -1);
+        if(lastElement == 0){
+            sb.delete(sb.length() - 1, sb.length()); 
+        } else if(x<0){
+            // sb.delete(lastElement, lastElement + 1);
+            sb.insert(0, '-');
+        }
+         try {
+            return Integer.parseInt(sb.toString()); 
+        } catch (NumberFormatException e) {
+            return 0; 
+        }        
+    }
+}
