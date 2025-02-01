a59b17dfbcfe78e99658e509bddd913f47bc8d4d
ermadhav <madhavtiwari2024@gmail.com>
2025-02-02 02:30:42 +0530

    Solved

---
+++ 3151.Special_Array_I.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        if (nums.length == 1) {
+            return true;
+        }
+        
+        for (int i = 0; i < nums.length - 1; i++) { // Avoid out-of-bounds error
+            if (nums[i] % 2 == nums[i + 1] % 2) { // Adjacent numbers should not have the same parity
+                return false;
+            }
+        }
+        
+        return true; // If all pairs follow the pattern, return true
+    }
+}
