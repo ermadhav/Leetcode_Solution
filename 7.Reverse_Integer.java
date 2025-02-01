27bd561f90fac18027bf7d8f446b95f24fe76985
ermadhav <madhavtiwari2024@gmail.com>
2025-02-01 11:37:55 +0530

    comment

--- 7.Reverse_Integer.java
+++ 7.Reverse_Integer.java
@@ -1,17 +1,19 @@
 class Solution {
     public int reverse(int x) {
-        StringBuilder sb = new StringBuilder(String.valueOf(Math.abs(x)));
+        StringBuilder sb = new StringBuilder(String.valueOf(Math.abs(x))); // Convert int to String
         sb.reverse();
-        int lastElement = sb.charAt(sb.length() -1);
-        if(lastElement == 0){
-            sb.delete(sb.length() - 1, sb.length()); 
-        } else if(x<0){
-            sb.insert(0, '-');
+        char lastElement = sb.charAt(sb.length() - 1); // Get last character
+
+        if (lastElement == '0') { // Check if last character is '0'
+            sb.delete(sb.length() - 1, sb.length()); // Remove trailing zero
+        } else if (x < 0) { // If original number was negative
+            sb.insert(0, '-'); // Prepend '-'
         }
-         try {
-            return Integer.parseInt(sb.toString()); 
+
+        try {
+            return Integer.parseInt(sb.toString()); // Convert back to int
         } catch (NumberFormatException e) {
-            return 0; 
-        }        
+            return 0; // Return 0 if overflow occurs
+        }
     }
 }
