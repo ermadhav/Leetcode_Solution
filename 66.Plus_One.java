777bc46e53f136c06928ebafeed80251ec1dcca8
ermadhav <madhavtiwari2024@gmail.com>
2025-02-02 03:03:41 +0530

    Solved

---
+++ 66.Plus_One.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        StringBuilder sb = new StringBuilder();
+
+        // Convert digits to a string and append to the StringBuilder
+        for (int i = 0; i < digits.length; i++) {
+            sb.append(digits[i]);
+        }
+        
+        // Convert the string to a number and add 1
+        int num = Integer.parseInt(sb.toString()) + 1;
+
+        // Convert the number back to an array
+        String numStr = String.valueOf(num);
+        int[] result = new int[numStr.length()];
+        for (int i = 0; i < numStr.length(); i++) {
+            result[i] = numStr.charAt(i) - '0';
+        }
+        
+        return result;
+    }
+}
