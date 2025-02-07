848bcdf613810077dc78b6645ee117d04a4865d5
ermadhav <madhavtiwari2024@gmail.com>
2025-02-07 21:09:27 +0530

    Solved

---
+++ 1929.Concatenation_of_Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n]; // Initialize ans with twice the size of nums
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];        // Copy the original array
+            ans[i + n] = nums[i];    // Copy again to form the concatenation
+        }
+        return ans;
+    }
+}
