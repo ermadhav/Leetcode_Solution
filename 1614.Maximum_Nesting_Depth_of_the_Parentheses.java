abfc0b804d4661d2c142bb4ad56f282b2e7d07d5
ermadhav <madhavtiwari2024@gmail.com>
2025-02-07 18:17:10 +0530

    Comment

--- 1614.Maximum_Nesting_Depth_of_the_Parentheses.java
+++ 1614.Maximum_Nesting_Depth_of_the_Parentheses.java
@@ -2,7 +2,6 @@ class Solution {
     public int maxDepth(String s) {
         int count = 0;
         int maxDepth = 0;  // Track the maximum nesting depth
-
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
                 count++;
