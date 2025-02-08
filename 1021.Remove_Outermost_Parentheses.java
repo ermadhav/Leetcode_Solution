950a73bf640864700656bd3ce49ff6af459a259d
ermadhav <madhavtiwari2024@gmail.com>
2025-02-08 13:36:24 +0530

    Solved

---
+++ 1021.Remove_Outermost_Parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder res = new StringBuilder();
+        int depth = 0; // Track depth of parentheses
+
+        for (int i = 0; i < s.length(); i++) { // Fix: Use s.length() instead of res.length()
+            if (s.charAt(i) == '(') {
+                if (depth > 0) { // Ignore outermost '('
+                    res.append(s.charAt(i));
+                }
+                depth++;
+            } else { // s.charAt(i) == ')'
+                depth--;
+                if (depth > 0) { // Ignore outermost ')'
+                    res.append(s.charAt(i));
+                }
+            }
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.removeOuterParentheses("(()())(())")); // Output: "()()()"
+    }
+}
