2f9772f27d87b1cf3b0148e7726386ed2defbe76
ermadhav <madhavtiwari2024@gmail.com>
2025-02-09 15:06:37 +0530

    Solved

---
+++ 1190.Reverse_Substrings_Between_Each_Pair_of_Parentheses.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+class Solution {
+    public String reverseParentheses(String s) {
+        Stack<StringBuilder> stack = new Stack<>();
+        StringBuilder current = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                stack.push(current); // Save current string before '('
+                current = new StringBuilder(); // Start a new string for inside parentheses
+            } else if (ch == ')') {
+                StringBuilder reversed = current.reverse(); // Reverse the inside part
+                current = stack.pop().append(reversed); // Append to the previous part
+            } else {
+                current.append(ch); // Add characters normally
+            }
+        }
+
+        return current.toString(); // Final result
+    }
+}
