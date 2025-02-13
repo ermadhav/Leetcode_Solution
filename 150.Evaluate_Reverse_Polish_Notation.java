8afef9b8598894283cced04a0aa6a620e50a36e7
ermadhav <madhavtiwari2024@gmail.com>
2025-02-13 17:52:38 +0530

    Solved

---
+++ 150.Evaluate_Reverse_Polish_Notation.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>(); // Stack to store operands
+        
+        for (String token : tokens) {
+            // If the token is an operator, pop two operands and apply the operation
+            if ("+-*/".contains(token)) {
+                int b = stack.pop(); // Second operand
+                int a = stack.pop(); // First operand
+                
+                // Perform the operation based on the operator
+                switch (token) {
+                    case "+": stack.push(a + b); break; // Addition
+                    case "-": stack.push(a - b); break; // Subtraction
+                    case "*": stack.push(a * b); break; // Multiplication
+                    case "/": stack.push(a / b); break; // Integer division (truncates towards zero)
+                }
+            } else {
+                // If the token is a number, push it onto the stack
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        
+        return stack.pop(); // The final result is the last element in the stack
+    }
+}
