770be8003292c106b2ee44b080f45fde192f45cf
ermadhav <madhavtiwari2024@gmail.com>
2025-02-14 19:05:27 +0530

    Solved

---
+++ 13.Roman_to_Integer.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int romanToInt(String s) {
+        // Step 1: Create a HashMap to store Roman numeral values
+        HashMap<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int total = 0;
+        int prevValue = 0;
+
+        // Step 2: Traverse the string from right to left
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = romanMap.get(s.charAt(i));
+
+            // Step 3: If a smaller value comes before a larger one, subtract it
+            if (currentValue < prevValue) {
+                total -= currentValue;
+            } else {
+                total += currentValue;
+            }
+
+            // Step 4: Update previous value
+            prevValue = currentValue;
+        }
+
+        return total;
+    }
+}
