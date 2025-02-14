3ff6f215c2b16023c2b9192b37c9582ad90def37
ermadhav <madhavtiwari2024@gmail.com>
2025-02-14 19:11:56 +0530

    Solved

--- 3280.Convert_Date_to_Binary.java
+++ 3280.Convert_Date_to_Binary.java
@@ -9,11 +9,5 @@ class Solution {
                Integer.toBinaryString(Integer.parseInt(parts[2]));       // Convert day to binary
     }
 
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        
-        // Example usage
-        System.out.println(sol.dateToBinary("2024-02-14")); // Output: "11111100100-10-1110"
-        System.out.println(sol.dateToBinary("2000-12-31")); // Output: "11111010000-1100-11111"
-    }
+   
 }
