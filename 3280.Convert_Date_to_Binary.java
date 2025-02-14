b29c7c2aa3001f60f273c56e15b265bb4c7cf1d2
ermadhav <madhavtiwari2024@gmail.com>
2025-02-14 19:13:53 +0530

    Solved

--- 3280.Convert_Date_to_Binary.java
+++ 3280.Convert_Date_to_Binary.java
@@ -4,9 +4,10 @@ class Solution {
         String[] parts = date.split("-");
 
         // Step 2: Convert each part to binary and concatenate with '-'
+        
         return Integer.toBinaryString(Integer.parseInt(parts[0])) + "-" + // Convert year to binary
                 Integer.toBinaryString(Integer.parseInt(parts[1])) + "-" + // Convert month to binary
                 Integer.toBinaryString(Integer.parseInt(parts[2])); // Convert day to binary
-                
+
     }
 }
