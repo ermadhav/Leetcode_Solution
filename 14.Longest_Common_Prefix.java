5e23f01884ac8260615a6b0100539093b91bfaf4
ermadhav <madhavtiwari2024@gmail.com>
2025-02-17 18:28:19 +0530

    Solved

--- 14.Longest_Common_Prefix.java
+++ 14.Longest_Common_Prefix.java
@@ -1,4 +1,5 @@
 class Solution {
+    
     public String longestCommonPrefix(String[] strs) {
         // If the input array is empty or null, return an empty string
         if (strs == null || strs.length == 0) {
