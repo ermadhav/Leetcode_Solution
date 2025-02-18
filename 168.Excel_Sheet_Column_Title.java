48a5500cfc44cef968091d9523aad7c57c4ab38a
ermadhav <madhavtiwari2024@gmail.com>
2025-02-18 22:05:42 +0530

    Solved

--- 168.Excel_Sheet_Column_Title.java
+++ 168.Excel_Sheet_Column_Title.java
@@ -1,8 +1,9 @@
 class Solution {
     public String convertToTitle(int columnNumber) {
+        
         StringBuilder sb = new StringBuilder();
         // Loop until columnNumber becomes 0
-        
+
         while (columnNumber > 0) {
             columnNumber--; // Adjusting for 0-based indexing
             // Get the last character (A-Z)
