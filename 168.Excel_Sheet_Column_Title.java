3502086b5cf8c54ce1998d04aaa2c7a8e57be832
ermadhav <madhavtiwari2024@gmail.com>
2025-02-18 22:06:22 +0530

    Solved

--- 168.Excel_Sheet_Column_Title.java
+++ 168.Excel_Sheet_Column_Title.java
@@ -3,11 +3,10 @@ class Solution {
 
         StringBuilder sb = new StringBuilder();
         // Loop until columnNumber becomes 0
-
         while (columnNumber > 0) {
 
             columnNumber--; // Adjusting for 0-based indexing
-            
+
             // Get the last character (A-Z)
             char ch = (char) ('A' + (columnNumber % 26)); 
             sb.append(ch);
