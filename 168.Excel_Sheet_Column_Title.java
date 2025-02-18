8bd89fefd24c39244502b966eeda429444825ac6
ermadhav <madhavtiwari2024@gmail.com>
2025-02-18 22:04:53 +0530

    Solved

--- 168.Excel_Sheet_Column_Title.java
+++ 168.Excel_Sheet_Column_Title.java
@@ -1,7 +1,6 @@
 class Solution {
     public String convertToTitle(int columnNumber) {
         StringBuilder sb = new StringBuilder();
-
         // Loop until columnNumber becomes 0
         while (columnNumber > 0) {
             columnNumber--; // Adjusting for 0-based indexing
