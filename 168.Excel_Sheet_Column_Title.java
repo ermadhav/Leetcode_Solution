dec6f08280d907fa44180df061df14ac514c1f28
ermadhav <madhavtiwari2024@gmail.com>
2025-02-18 22:05:58 +0530

    Solved

--- 168.Excel_Sheet_Column_Title.java
+++ 168.Excel_Sheet_Column_Title.java
@@ -5,11 +5,12 @@ class Solution {
         // Loop until columnNumber becomes 0
 
         while (columnNumber > 0) {
+            
             columnNumber--; // Adjusting for 0-based indexing
             // Get the last character (A-Z)
             char ch = (char) ('A' + (columnNumber % 26)); 
             sb.append(ch);
-            
+
             // Move to the next position in sequence
             columnNumber /= 26;
         }
