60c56409debd406c1ef272c398e12c43ae815b29
ermadhav <madhavtiwari2024@gmail.com>
2025-02-23 13:59:27 +0530

    updated

--- 263.Ugly_Number.java
+++ 263.Ugly_Number.java
@@ -1,19 +1,12 @@
-public class UglyNumber {
-    public static boolean isUgly(int n) {
-
-        if (n <= 0) return false; // Ugly numbers must be positive
-        
-        // Divide n by 2, 3, and 5 as much as possible
-        int[] factors = {2, 3, 5};
-
-        // Keep dividing n by 2, 3, and 5 as long as it is divisible
-        for (int factor : factors) {
-            while (n % factor == 0) { // Check divisibility
-                n /= factor; // Reduce n by dividing it by the current factor
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<=0) return false;
+        int[] factors = {2,3,5};
+        for(int factor: factors){
+            while(n%factor == 0){
+                n /= factor;
             }
         }
-        
-        // If the remaining number is 1, it's an ugly number
-        return n == 1;
+        return n ==1;
     }
 }
