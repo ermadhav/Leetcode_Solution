04d0be14086252965437fb8578854f698c77f67c
ermadhav <madhavtiwari2024@gmail.com>
2025-02-28 09:55:35 +0530

    Solved

--- 326.Power_of_Three.java
+++ 326.Power_of_Three.java
@@ -9,7 +9,6 @@ class Solution {
         while (n % 3 == 0) {
             n /= 3; // Reduce n by dividing it by 3
         }
-        
         // If n becomes 1, then it is a power of three; otherwise, it's not
         return n == 1;
     }
