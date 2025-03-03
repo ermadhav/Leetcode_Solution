d48612f9ae11d20e2c2e260fb9dcbb0cb50bb62a
ermadhav <madhavtiwari2024@gmail.com>
2025-03-03 17:18:27 +0530

    SOlved

---
+++ 83.Remove_Duplicates_from_Sorted_List.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;  // Pointer to traverse the linked list
+
+        while (curr != null && curr.next != null) { // Traverse until the end
+            if (curr.val == curr.next.val) { 
+                // If duplicate found, skip the next node
+                curr.next = curr.next.next;
+            } else {
+                // Move to the next node
+                curr = curr.next;
+            }
+        }
+
+        return head;  // Return the updated list without duplicates
+    }
+}
