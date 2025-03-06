369517e7bf6db90521f21238ea50917e26fac27e
ermadhav <madhavtiwari2024@gmail.com>
2025-03-06 19:08:44 +0530

    Solved

--- oddEvenLinkedList.java
+++ oddEvenLinkedList.java
@@ -7,7 +7,7 @@ class ListNode {
     }
 }
 
-public class Solution {
+public class oddEvenLinkedList {
     public ListNode oddEvenList(ListNode head) {
         if (head == null || head.next == null) {
             return head; // If the list is empty or has only one node, return as is.
@@ -20,7 +20,7 @@ public class Solution {
         while (even != null && even.next != null) {
             odd.next = even.next; // Link the current odd node to the next odd node
             odd = odd.next; // Move odd pointer
-            
+
             even.next = odd.next; // Link the current even node to the next even node
             even = even.next; // Move even pointer
         }
