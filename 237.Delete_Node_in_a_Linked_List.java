2bfca893f2d15a349c2d45b033ac3cc08292a1a4
ermadhav <madhavtiwari2024@gmail.com>
2025-03-08 16:01:39 +0530

    Solved

---
+++ 237.Delete_Node_in_a_Linked_List.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void deleteNode(ListNode node) {
+        // Copy the value of the next node into the current node
+        node.val = node.next.val;
+        
+        // Update the next pointer to skip the next node
+        node.next = node.next.next;
+    }
+}
