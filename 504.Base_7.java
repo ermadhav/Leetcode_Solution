0d31a7c141a9f3c375322fc25123d547a5fe1b04
ermadhav <madhavtiwari2024@gmail.com>
2025-03-10 19:27:29 +0530

    Solved

--- 504.Base_7.java
+++ 504.Base_7.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Base_7 {
     public static String convertToBase7(int num) {
         // Using built-in Integer.toString() method with radix 7
         return Integer.toString(num, 7);
