39ae4b5ee588e0e0b0c49e8b3584b6d2fb114d69
ermadhav <madhavtiwari2024@gmail.com>
2025-03-12 19:48:31 +0530

    Solved

--- 82.Remove_Duplicates_from_Sorted_List_II.java
+++ 82.Remove_Duplicates_from_Sorted_List_II.java
@@ -1,8 +1,8 @@
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
-}
+// class ListNode {
+//     int val;
+//     ListNode next;
+//     ListNode(int x) { val = x; }
+// }
 
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
