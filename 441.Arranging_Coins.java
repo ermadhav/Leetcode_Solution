7c8405d25fc8358040dc64cab09a688a2c3a9b1f
ermadhav <madhavtiwari2024@gmail.com>
2025-03-18 16:08:28 +0530

    Solved

--- 441.Arranging_Coins.java
+++ 441.Arranging_Coins.java
@@ -1,7 +1,8 @@
 class Solution {
     public int arrangeCoins(int n) {
-        
+
         // Using the quadratic formula: k = (-1 + sqrt(1 + 8n)) / 2
         return (int) ((Math.sqrt(1 + 8L * n) - 1) / 2);
+        
     }
 }
