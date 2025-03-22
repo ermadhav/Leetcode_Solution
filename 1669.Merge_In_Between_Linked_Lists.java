033ba8c41b82af35d399d45f1d91b029584fe750
ermadhav <madhavtiwari2024@gmail.com>
2025-03-22 17:51:34 +0530

    Solved

--- 1669.Merge_In_Between_Linked_Lists.java
+++ 1669.Merge_In_Between_Linked_Lists.java
@@ -1,26 +1,39 @@
-import java.util.List;
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 
 class Solution {
-    public List mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
-        ListNode prevA = list1;  // Pointer to (a-1)th node
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode prevA = list1;
         for (int i = 0; i < a - 1; i++) {
             prevA = prevA.next;
         }
 
-        ListNode afterB = prevA.next;  // Pointer to (b+1)th node
+        ListNode afterB = prevA.next;
         for (int i = 0; i < (b - a + 1); i++) {
             afterB = afterB.next;
         }
 
-        prevA.next = list2;  // Link (a-1)th node to list2 head
+        prevA.next = list2;
 
-        ListNode tail2 = list2;  // Find the last node of list2
+        ListNode tail2 = list2;
         while (tail2.next != null) {
             tail2 = tail2.next;
         }
 
-        tail2.next = afterB;  // Link list2's tail to (b+1)th node
+        tail2.next = afterB;
 
-        return list1;  // Return modified list1
+        return list1;
     }
 }
