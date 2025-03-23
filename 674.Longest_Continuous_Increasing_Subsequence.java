5cc2d340283a27ce5ee5d293a05c9bd287132839
ermadhav <madhavtiwari2024@gmail.com>
2025-03-23 16:11:28 +0530

    Solved

--- 674.Longest_Continuous_Increasing_Subsequence.java
+++ 674.Longest_Continuous_Increasing_Subsequence.java
@@ -1,1 +1,16 @@
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        int maxLength = 1, count = 1; // Track longest and current sequence length
 
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] > nums[i - 1]) { // If the sequence continues
+                count++; // Increase count
+                maxLength = Math.max(maxLength, count); // Update max length
+            } else {
+                count = 1; // Reset count if sequence breaks
+            }
+        } 
+
+        return maxLength; // Return longest increasing sequence length
+    }
+}
