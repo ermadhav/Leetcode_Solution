4e451add1cd48642ba0cf0ec73ef4b992b161bd4
ermadhav <madhavtiwari2024@gmail.com>
2025-03-24 17:27:46 +0530

    Solvedd

--- 717.1-bit_and_2-bit_Characters.java
+++ 717.1-bit_and_2-bit_Characters.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
-        int l = bits.length;
+        int l = bits.length; // Length the bits Array
         int i = 0;
 
         while (i < l - 1) { // Traverse until the second-last character
