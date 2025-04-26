a0b23da335b067cc4f15743496c214062a9338af
ermadhav <madhavtiwari2024@gmail.com>
2025-04-26 18:15:50 +0530

    Solved

--- 1502.Can_Make_Arithmetic_Progression_From_Sequence.java
+++ 1502.Can_Make_Arithmetic_Progression_From_Sequence.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
         Arrays.sort(arr);
-        int diff = arr[1]-arr[0];
-        for(int i=1; i<arr.length-1; i++){
-            if(arr[i+1]-arr[i] != diff){
-                return false;
+        int diff = arr[1] - arr[0];  // Notice: arr[1] - arr[0]
+
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i+1] - arr[i] != diff) {
+                return false;  // Immediately return false if any gap is wrong
             }
         }
-        return true;
+        return true;  // If loop finishes, it's a valid arithmetic progression
     }
 }
