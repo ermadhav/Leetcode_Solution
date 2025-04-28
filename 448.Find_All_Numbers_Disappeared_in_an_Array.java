c66ba95d32bef44444001ea4b2961f3ca7ed3249
ermadhav <madhavtiwari2024@gmail.com>
2025-04-28 19:37:52 +0530

    Solved

---
+++ 448.Find_All_Numbers_Disappeared_in_an_Array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        
+        // Step 1: Mark numbers that appear by negating the value at the index
+        for (int i = 0; i < nums.length; i++) {
+            int num = Math.abs(nums[i]);
+            if (nums[num - 1] > 0) {
+                nums[num - 1] = -nums[num - 1];
+            }
+        }
+        
+        // Step 2: Collect missing numbers (positive values mean missing)
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+        
+        return result;
+    }
+}
