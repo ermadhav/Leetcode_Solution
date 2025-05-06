560a7a780e8a7a93f4cad16c0d45d101fb6eadde
ermadhav <madhavtiwari2024@gmail.com>
2025-05-06 10:40:14 +0530

    Solved

---
+++ 1431.Kids_With_the_Greatest_Number_of_Candies.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        // Step 1: Find the true max first
+        int max = candies[0];
+        for(int i = 1; i < candies.length; i++) {
+            if(candies[i] > max){
+                max = candies[i];
+            }
+        }
+
+        // Step 2: Build result list
+        List<Boolean> result = new ArrayList<>();
+        for(int i = 0; i < candies.length; i++) {
+            if(candies[i] + extraCandies >= max){
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+        }
+
+        return result;
+    }
+}
