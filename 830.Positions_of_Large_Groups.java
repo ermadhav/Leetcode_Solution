35c0f34c8c2a3503438c8f81c9f0ad511b0c2466
ermadhav <madhavtiwari2024@gmail.com>
2025-05-11 15:49:46 +0530

    Solved

--- 830.Positions_of_Large_Groups.java
+++ 830.Positions_of_Large_Groups.java
@@ -4,16 +4,19 @@ import java.util.Arrays;
 class Solution {
     public List<List<Integer>> largeGroupPositions(String s) {
         List<List<Integer>> result = new ArrayList<>();
-        int start = 0;
-        for(int i=0; i<s.length(); i++){
-            if( i== s.length()-1 || s.charAt(i) != s.charAt(i+1)){
-                int groupLength = i- start +1;
-                if(groupLength >= 3){
+        int start = 0;  // Start of current group
+
+        for (int i = 0; i < s.length(); i++) {
+            // Check if it's the end of the group (last char or char changes)
+            if (i == s.length() - 1 || s.charAt(i) != s.charAt(i + 1)) {
+                int groupLength = i - start + 1;
+                if (groupLength >= 3) {
                     result.add(Arrays.asList(start, i));
                 }
-                start = i+1;
+                start = i + 1;  // Start of next group
             }
         }
+
         return result;
     }
 }
