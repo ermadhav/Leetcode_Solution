d93e8e3ed8215ca742691e38bfd0346b56c1cab4
ermadhav <madhavtiwari2024@gmail.com>
2025-05-14 18:41:54 +0530

    Solved

--- 2000.Reverse_Prefix_of_Word.java
+++ 2000.Reverse_Prefix_of_Word.java
@@ -1,14 +1,12 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
-        int index = word.indexOf(ch);  // Find the index of the first occurrence of ch
+        int index = word.indexOf(ch);
         if (index == -1) {
-            return word;  // If ch is not found, return the word as is
+            return word; 
         }
-
-        // Reverse the substring from 0 to index (inclusive)
-        StringBuilder sb = new StringBuilder(word.substring(0, index + 1));
+        StringBuilder sb = new StringBuilder(word.substring(0, index+1));
         sb.reverse();
-        sb.append(word.substring(index + 1));  // Append the rest of the word
+        sb.append(word.substring(index+1));
 
         return sb.toString();
     }
