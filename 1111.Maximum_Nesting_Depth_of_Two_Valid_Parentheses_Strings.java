612466529709ad0bd17c7db35379d8d65dba9658
ermadhav <madhavtiwari2024@gmail.com>
2025-05-14 18:47:12 +0530

    Solved

---
+++ 1111.Maximum_Nesting_Depth_of_Two_Valid_Parentheses_Strings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] maxDepthAfterSplit(String seq) {
+        int[] res = new int[seq.length()];
+        int depth = 0;
+
+        for (int i = 0; i < seq.length(); i++) {
+            if (seq.charAt(i) == '(') {
+                res[i] = depth % 2; // Alternate groups based on current depth
+                depth++;
+            } else {
+                depth--;
+                res[i] = depth % 2; // Match closing with correct group
+            }
+        }
+
+        return res;
+    }
+}
