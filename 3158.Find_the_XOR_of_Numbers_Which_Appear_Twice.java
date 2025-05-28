1495985fe9368b2b1cd07162f2170e02cd8689fc
ermadhav <madhavtiwari2024@gmail.com>
2025-05-28 19:53:05 +0530

    Solved

---
+++ 3158.Find_the_XOR_of_Numbers_Which_Appear_Twice.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int duplicateNumbersXOR(int[] nums) {
+int result = 0;
+    Set<Integer> seen = new HashSet<>();
+    
+    for (int num : nums) {
+        // If number is already seen once, it means this is the second occurrence (duplicate)
+        if (seen.contains(num)) {
+            // XOR the duplicate number into result
+            result ^= num;
+        } else {
+            // First time seeing this number, add it to the set
+            seen.add(num);
+        }
+    }
+    
+    // Return XOR of all numbers that appear twice
+    return result;
+    }
+}
