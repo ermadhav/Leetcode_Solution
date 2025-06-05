8915d59f83050a5213db780a07115b0a25beb487
ermadhav <madhavtiwari2024@gmail.com>
2025-06-05 13:18:01 +0530

    Solved

---
+++ 3153.Sum_of_Digit_Differences_of_All_Pairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long sumDigitDifferences(int[] nums) {
+        int n = nums.length;
+        int len = String.valueOf(nums[0]).length();
+        int total = 0;
+
+        for (int pos = 0; pos < len; pos++) {
+            int[] count = new int[10];
+            for (int num : nums) {
+                int digit = String.valueOf(num).charAt(pos) - '0';
+                count[digit]++;
+            }
+            for (int i = 0; i < 10; i++) {
+                for (int j = i + 1; j < 10; j++) {
+                    total += count[i] * count[j];
+                }
+            }
+        }
+
+        return total;
+    }
+}
