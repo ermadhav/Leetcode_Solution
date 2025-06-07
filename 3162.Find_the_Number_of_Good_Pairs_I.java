ebb32f1bf3a14b1dc0aed409a9a7876a4856a487
ermadhav <madhavtiwari2024@gmail.com>
2025-06-07 13:13:08 +0530

    Solved

---
+++ 3162.Find_the_Number_of_Good_Pairs_I.java
@@ -0,0 +1,22 @@
+import java.util.Map;
+
+class Solution {
+    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        // Build a frequency map of nums2[j] * k
+        for (int num : nums2) {
+            int product = num * k;
+            map.put(product, map.getOrDefault(product, 0) + 1);
+        }
+        int count = 0;
+        // For each element in nums1, check how many values from nums2 * k divide it
+        for (int a : nums1) {
+            for (int b : map.keySet()) {
+                if (a % b == 0) {
+                    count += map.get(b); // Add number of such valid b's
+                }
+            }
+        }
+        return count;
+    }
+}
