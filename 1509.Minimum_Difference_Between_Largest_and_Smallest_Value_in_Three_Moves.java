a9c7bfe0805ec6a845740853c88123ad002d75b8
ermadhav <madhavtiwari2024@gmail.com>
2025-06-14 02:24:44 +0530

    Solved

---
+++ 1509.Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minDifference(int[] nums) {
+         int n = nums.length;
+        if (n <= 4) {
+            return 0;  // Can change all elements to be equal
+        }
+        
+        Arrays.sort(nums);
+        
+        // Check the 4 possible ways after changing up to 3 elements
+        return Math.min(
+            Math.min(nums[n - 4] - nums[0], nums[n - 3] - nums[1]),
+            Math.min(nums[n - 2] - nums[2], nums[n - 1] - nums[3])
+        );
+    }
+}
