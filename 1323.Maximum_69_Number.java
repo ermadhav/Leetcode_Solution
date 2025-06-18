88690f78849ac166aebe6d9aa757ed34f668085e
ermadhav <madhavtiwari2024@gmail.com>
2025-06-18 21:32:51 +0530

    Solved

---
+++ 1323.Maximum_69_Number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximum69Number(int num) {
+        // Convert the number to a char array
+        char[] digits = Integer.toString(num).toCharArray();
+
+        // Change the first '6' to '9' to maximize the number
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] == '6') {
+                digits[i] = '9';
+                break; // Only change one digit
+            }
+        }
+
+        // Convert the updated char array back to an integer
+        return Integer.parseInt(new String(digits));
+    }
+}
