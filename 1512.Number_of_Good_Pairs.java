3ac5e3c2b9b05f6b55ed224d8774b4f3a2ab5092
ermadhav <madhavtiwari2024@gmail.com>
2025-06-24 14:32:15 +0530

    Solvede

--- 1512.Number_of_Good_Pairs.java
+++ 1512.Number_of_Good_Pairs.java
@@ -1,13 +1,14 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
-        int count =0;
-        for (int i=0; i<nums.length; i++){
-            for(int j=0;j<nums.length; j++){
-                if(nums[i] == nums[j] && i<j){
-                    count++;
+        int count = 0; // To count the number of good pairs
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                // Check if elements are equal and indices form a valid pair (i < j)
+                if (nums[i] == nums[j] && i < j) {
+                    count++; // Increment count for each good pair
                 }
             }
         }
-        return count;
+        return count; // Return total number of good pairs
     }
 }
