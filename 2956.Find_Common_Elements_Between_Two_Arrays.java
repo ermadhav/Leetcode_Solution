df1e8e56a26c2a0228d8cac63be1a65a43fa0477
ermadhav <madhavtiwari2024@gmail.com>
2025-06-28 14:32:07 +0530

    Solved

---
+++ 2956.Find_Common_Elements_Between_Two_Arrays.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+
+public class Solution {
+    public int[] countMatches(int[] nums1, int[] nums2) {
+        // Store unique elements of nums1 and nums2
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+
+        for (int num : nums1) set1.add(num);
+        for (int num : nums2) set2.add(num);
+
+        int answer1 = 0;
+        // Count nums1 elements present in nums2
+        for (int num : nums1) {
+            if (set2.contains(num)) answer1++;
+        }
+
+        int answer2 = 0;
+        // Count nums2 elements present in nums1
+        for (int num : nums2) {
+            if (set1.contains(num)) answer2++;
+        }
+
+        // Return both counts
+        return new int[]{answer1, answer2};
+    }
+}
