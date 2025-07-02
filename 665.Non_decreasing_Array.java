9f442623c929e95295763da06ca9e2ba294c064f
ermadhav <madhavtiwari2024@gmail.com>
2025-07-02 21:10:09 +0530

    deleted everything

--- 665.Non_decreasing_Array.java
+++ 665.Non_decreasing_Array.java
@@ -1,3 +1,22 @@
-public class 665.Non_decreasing_Array {
-    
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int count = 0; // To count how many changes are needed
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            // If current element is greater than the next, it's a violation
+            if (nums[i] > nums[i + 1]) {
+                count++;
+                if (count > 1) return false; // More than one change needed
+
+                // Decide which element to modify
+                if (i == 0 || nums[i - 1] <= nums[i + 1]) {
+                    nums[i] = nums[i + 1]; // Modify current element
+                } else {
+                    nums[i + 1] = nums[i]; // Modify next element
+                }
+            }
+        }
+        
+        return true; // At most one modification needed
+    }
 }
