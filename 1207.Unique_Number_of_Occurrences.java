ee150ff79e79487e08b9334500ff26cca428cb0a
ermadhav <madhavtiwari2024@gmail.com>
2025-07-06 12:52:23 +0530

    Solved

--- 1207.Unique_Number_of_Occurrences.java
+++ 1207.Unique_Number_of_Occurrences.java
@@ -1,15 +1,21 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
         Map<Integer, Integer> countMap = new HashMap<>();
-        for(int num: arr){
-            countMap.put(num, countMap.getOrDefault(num, 0)+1);
+
+        // Count frequency of each number
+        for (int num : arr) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
+
         Set<Integer> occurrenceSet = new HashSet<>();
-        for(int count: countMap.values()){
-            if(!occurrenceSet.add(count)){
-                return false;
+
+        // Check if all frequencies are unique
+        for (int count : countMap.values()) {
+            if (!occurrenceSet.add(count)) {
+                return false; // Duplicate frequency found
             }
         }
-        return true;
+
+        return true; // All frequencies are unique
     }
 }
