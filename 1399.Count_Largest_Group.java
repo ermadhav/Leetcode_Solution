20f52ef114c51bfbf0e328db8d79313fb2edf880
ermadhav <madhavtiwari2024@gmail.com>
2025-07-06 13:08:03 +0530

    Solved

--- 1399.Count_Largest_Group.java
+++ 1399.Count_Largest_Group.java
@@ -1,25 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int countLargestGroup(int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // Count frequency of digit sums
         for (int i = 1; i <= n; i++) {
             int sum = digitSum(i);
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
+
+        // Find max group size
         int max = 0;
         for (int val : map.values()) {
-            if (val > max)
-                max = val;
+            if (val > max) max = val;
         }
 
+        // Count how many groups have max size
         int count = 0;
         for (int val : map.values()) {
-            if (val == max)
-                count++;
+            if (val == max) count++;
         }
 
         return count;
-
     }
 
+    // Helper method to calculate digit sum
     private int digitSum(int num) {
         int sum = 0;
         while (num > 0) {
