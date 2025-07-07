1b5b1c92a6ae1d15d95a3b5b5664068c58df0911
ermadhav <madhavtiwari2024@gmail.com>
2025-07-07 15:16:55 +0530

    olved

--- 1812.Determine_Color_of_a_Chessboard_Square.java
+++ 1812.Determine_Color_of_a_Chessboard_Square.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean squareIsWhite(String coordinates) {
-        char column = coordinates.charAt(0); // e.g., 'a'
-        char row = coordinates.charAt(1);    // e.g., '1'
+        char column = coordinates.charAt(0); 
+        char row = coordinates.charAt(1);    
         
-        int colIndex = column - 'a' + 1; // 'a' = 1, 'b' = 2, ..., 'h' = 8
-        int rowIndex = row - '0';        // '1' = 1, ..., '8' = 8
+        int colIndex = column - 'a' + 1; 
+        int rowIndex = row - '0';        
 
         return (colIndex + rowIndex) % 2 != 0;
     }
