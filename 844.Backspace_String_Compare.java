7d2eb8c637eb4f22deb31877ab7c85c546519952
ermadhav <madhavtiwari2024@gmail.com>
2025-07-10 13:00:25 +0530

    Solved

--- 844.Backspace_String_Compare.java
+++ 844.Backspace_String_Compare.java
@@ -1,20 +1,19 @@
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
-        return build(s).equals(build(t));
+    public static boolean backspaceCompare(String s, String t) {
+        return build(s).equals(build(t)); // Compare final processed forms
     }
-    private static String build(String str){
-        Stack<Character> st = new Stack<>();
-        for(char c : str.toCharArray()){
-            if(c != '#'){
-                st.push(c);
-            }else if(!st.isEmpty()){
-                st.pop();
-            }
+
+    // Simulate typing with backspace using a stack
+    private static String build(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            if (c != '#') stack.push(c);       // Add normal character
+            else if (!stack.isEmpty()) stack.pop(); // Remove last character
         }
+
+        // Convert stack to string
         StringBuilder sb = new StringBuilder();
-        for(char c : st) {
-            sb.append(c);
-        }
+        for (char c : stack) sb.append(c);
         return sb.toString();
     }
 }
