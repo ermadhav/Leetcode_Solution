282b11de32667cccdc0471ad04d85ee7154f5eb4
ermadhav <madhavtiwari2024@gmail.com>
2025-07-11 23:51:22 +0530

    Solved

---
+++ 2651.Calculate_Delayed_Arrival_Time.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        // Calculate the raw arrival time by adding delay to the original arrival time
+        int num = arrivalTime + delayedTime;
+        
+        // If the time goes beyond 24 hours (i.e., next day), wrap it around using 24-hour clock format
+        if (num >= 24) {
+            num = num - 24;
+        }
+
+        // Return the adjusted arrival time
+        return num;
+    }
+}
