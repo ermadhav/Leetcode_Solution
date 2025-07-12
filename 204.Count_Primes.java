6d83d3b736cfa3b816d02b23829e8199d0f54457
ermadhav <madhavtiwari2024@gmail.com>
2025-07-12 12:51:18 +0530

    Solved

--- 204.Count_Primes.java
+++ 204.Count_Primes.java
@@ -1,14 +1,20 @@
 class Solution {
     public int countPrimes(int n) {
+        // Create a boolean array to mark non-prime numbers
         boolean[] prime = new boolean[n];
         int count = 0;
+
+        // Loop through numbers from 2 to n-1
         for (int i = 2; i < n; i++) {
+            // If i is not marked, it's a prime
             if (!prime[i]) {
-                count++;
+                count++; // Count the prime
+
+                // Mark all multiples of i as non-prime
                 for (int j = i * 2; j < n; j += i)
                     prime[j] = true;
             }
         }
-        return count;
+        return count; // Return number of primes less than n
     }
 }
