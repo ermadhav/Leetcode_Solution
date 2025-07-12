71ec5ebd17febf8c7bdb47e27492ad7a79914502
ermadhav <madhavtiwari2024@gmail.com>
2025-07-12 22:31:45 +0530

    Solved

--- 2169.Count_Operations_to_Obtain_Zero.java
+++ 2169.Count_Operations_to_Obtain_Zero.java
@@ -1,14 +1,21 @@
 class Solution {
     public int countOperations(int num1, int num2) {
-        int count =0;
-        while(num1 != 0 && num2 !=0){
-            if(num1 >= num2){
-                num1 = num1-num2;
-            }else{
-                num2 = num2-num1;
+        int count = 0; // Initialize a counter to track the number of operations
+
+        // Continue looping until either num1 or num2 becomes 0
+        while (num1 != 0 && num2 != 0) {
+            // If num1 is greater than or equal to num2, subtract num2 from num1
+            if (num1 >= num2) {
+                num1 = num1 - num2;
+            } else {
+                // If num2 is greater than num1, subtract num1 from num2
+                num2 = num2 - num1;
             }
-            count++;
+
+            count++; // Increment the operation count after each subtraction
         }
+
+        // Return the total number of operations performed
         return count;
     }
 }
