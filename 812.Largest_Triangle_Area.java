cd4806fd0155bac9ab5efcd05bdeac391d1db1f9
ermadhav <madhavtiwari2024@gmail.com>
2025-07-16 00:42:01 +0530

    Solved

--- 812.Largest_Triangle_Area.java
+++ 812.Largest_Triangle_Area.java
@@ -1,6 +1,6 @@
 class Solution {
     public double largestTriangleArea(int[][] points) {
-double maxArea = 0;
+        double maxArea = 0;
 
         // Loop through all combinations of 3 different points
         for (int i = 0; i < points.length; i++) {
@@ -13,15 +13,15 @@ double maxArea = 0;
             }
         }
 
-        return maxArea;  // Return the largest area found
+        return maxArea; // Return the largest area found
     }
 
     // Helper method to calculate triangle area using the Shoelace formula
     private static double area(int[] a, int[] b, int[] c) {
         return 0.5 * Math.abs(
-            a[0] * (b[1] - c[1]) +   // x1(y2 - y3)
-            b[0] * (c[1] - a[1]) +   // x2(y3 - y1)
-            c[0] * (a[1] - b[1])     // x3(y1 - y2)
+                a[0] * (b[1] - c[1]) + // x1(y2 - y3)
+                b[0] * (c[1] - a[1]) + // x2(y3 - y1)
+                c[0] * (a[1] - b[1]) // x3(y1 - y2)
         );
     }
 }
