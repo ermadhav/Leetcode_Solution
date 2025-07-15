ea0d2202cdb5729ed2a715e84c8a881e7e9fa18d
ermadhav <madhavtiwari2024@gmail.com>
2025-07-16 00:54:00 +0530

    Solved

--- 1037.Valid_Boomerang.java
+++ 1037.Valid_Boomerang.java
@@ -1,8 +1,11 @@
 class Solution {
     public boolean isBoomerang(int[][] points) {
-        int area = points[0][0]*(points[1][1] - points[2][1]) +
-                   points[1][0]*(points[2][1] - points[0][1]) +
-                   points[2][0]*(points[0][1] - points[1][1]);
+        // Compute 2 * area of triangle formed by the 3 points using the shoelace formula
+        int area = p[0][0] * (p[1][1] - p[2][1]) +   // x1 * (y2 - y3)
+                   p[1][0] * (p[2][1] - p[0][1]) +   // x2 * (y3 - y1)
+                   p[2][0] * (p[0][1] - p[1][1]);    // x3 * (y1 - y2)
+
+        // If the area is not zero, the points are not colinear ⇒ it's a boomerang
         return area != 0;
     }
 }
