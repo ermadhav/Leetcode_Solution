d2ea1f4453c832fbb111a60a0eab9bfdbe33c691
ermadhav <madhavtiwari2024@gmail.com>
2025-07-15 16:17:52 +0530

    Solved

--- 836.Rectangle_Overlap.java
+++ 836.Rectangle_Overlap.java
@@ -1,8 +1,10 @@
 class Solution {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        if(rec1[2] <= rec2[0] || rec2[2] <= rec1[0] || rec1[3] <= rec2[1] || rec2[3] <= rec1[1]){
+        // If one rectangle is left of the other, If one rectangle is above the other,
+        // no overlap
+        if (rec1[2] <= rec2[0] || rec2[2] <= rec1[0] || rec1[3] <= rec2[1] || rec2[3] <= rec1[1]) {
             return false;
-        }else{
+        } else {
             return true;
         }
     }
