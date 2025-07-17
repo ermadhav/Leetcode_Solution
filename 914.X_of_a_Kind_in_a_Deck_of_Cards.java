55c40e570fc09e7a614f3220674b36c363752c82
ermadhav <madhavtiwari2024@gmail.com>
2025-07-17 23:56:18 +0530

    Solved

--- 914.X_of_a_Kind_in_a_Deck_of_Cards.java
+++ 914.X_of_a_Kind_in_a_Deck_of_Cards.java
@@ -1,16 +1,20 @@
 class Solution {
     public boolean hasGroupsSizeX(int[] deck) {
+         // Count each number's frequency
         Map<Integer, Integer> countMap = new HashMap<>();
         for (int card : deck)
             countMap.put(card, countMap.getOrDefault(card, 0) + 1);
 
+        // Find GCD of all frequencies
         int gcd = 0;
         for (int count : countMap.values())
             gcd = computeGCD(gcd, count);
 
+        // Valid if GCD > 1
         return gcd > 1;
     }
 
+    // GCD function
     private int computeGCD(int a, int b) {
         return b == 0 ? a : computeGCD(b, a % b);
     }
