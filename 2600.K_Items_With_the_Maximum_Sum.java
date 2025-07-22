49e36cda19a155d698072d258db9921495a99721
ermadhav <madhavtiwari2024@gmail.com>
2025-07-22 17:59:44 +0530

    Solved

--- 2600.K_Items_With_the_Maximum_Sum.java
+++ 2600.K_Items_With_the_Maximum_Sum.java
@@ -1,8 +1,13 @@
 class Solution {
     public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        // Pick as many 1s as possible (to maximize sum)
         int ones = Math.min(numOnes, k);
-        k -= ones;
-        k -= Math.min(numZeros, k);
-        return ones-k;
+        k -= ones; // reduce k by number of 1s picked
+
+        // Pick as many 0s as needed (sum stays same)
+        k -= Math.min(numZeros, k); // reduce k by zeros picked
+
+        // Remaining k items must be -1s, subtract them from sum
+        return ones - k;
     }
 }
