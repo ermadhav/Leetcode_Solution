def421e0e8c62da433dec3852755b64735031c95
ermadhav <madhavtiwari2024@gmail.com>
2025-07-24 20:22:32 +0530

    Solved

--- 1362.Closest_Divisors.java
+++ 1362.Closest_Divisors.java
@@ -1,11 +1,13 @@
 class Solution {
     public int[] closestDivisors(int num) {
-        int a = num+1;
-        int b = num+2;
-        for(int i= (int)Math.sqrt(b); i >= 1; i--){
-            if(a % i == 0) return new int[]{i, a/i};
-            if(b % i == 0) return new int[]{i, b/i};
+        // Start from sqrt(b) and go down to 1 to find closest factors
+        for (int i = (int)Math.sqrt(b); i >= 1; i--) {
+            // If i divides a, return the factor pair (i, a/i)
+            if (a % i == 0) return new int[]{i, a / i};
+            // If i divides b, return the factor pair (i, b/i)
+            if (b % i == 0) return new int[]{i, b / i};
         }
+        // Fallback return, though we’ll always find a valid pair before this
         return null;
     }
 }
