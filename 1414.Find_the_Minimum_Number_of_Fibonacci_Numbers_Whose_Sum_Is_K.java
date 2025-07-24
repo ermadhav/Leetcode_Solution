471fa1cfe27a7aae38b3b23982689468ca674c0c
ermadhav <madhavtiwari2024@gmail.com>
2025-07-24 15:17:50 +0530

    Solved

--- 1414.Find_the_Minimum_Number_of_Fibonacci_Numbers_Whose_Sum_Is_K.java
+++ 1414.Find_the_Minimum_Number_of_Fibonacci_Numbers_Whose_Sum_Is_K.java
@@ -1,23 +1,27 @@
 class Solution {
     public int findMinFibonacciNumbers(int k) {
-        int a =1;
-        int b =1;
-        int count =0;
-        while(b <= k){
-            int temp = a+b;
+        int a = 1, b = 1, count = 0;
+        
+        // Generate the largest Fibonacci number <= k
+        while (b <= k) {
+            int temp = a + b;
             a = b;
             b = temp;
         }
-        while(k>0){
-            if(a <= k){
-                k -= a;
-                count++;
-            }else{
-                int temp = b-a;
+        
+        // Subtract Fibonacci numbers from k starting from the largest
+        while (k > 0) {
+            if (a <= k) {
+                k -= a;   // Use this Fibonacci number
+                count++;  // Increase count
+            } else {
+                // Move to the previous Fibonacci number by reversing the sum
+                int temp = b - a;
                 b = a;
                 a = temp;
             }
         }
-        return count;
+        
+        return count; // Return the minimum count
     }
 }
