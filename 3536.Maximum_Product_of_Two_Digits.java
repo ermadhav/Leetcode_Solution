cf49ae496c45910a481421a281607bef8868064c
ermadhav <madhavtiwari2024@gmail.com>
2025-07-24 15:00:02 +0530

    Solved

--- 3536.Maximum_Product_of_Two_Digits.java
+++ 3536.Maximum_Product_of_Two_Digits.java
@@ -1,29 +1,18 @@
 class Solution {
     public int maxProduct(int n) {
-        // Convert the integer n to a string so we can access each digit
-        String s = Integer.toString(n);
+        String numStr = Integer.toString(n);
+        int len = numStr.length();
+        int maxProduct = 0;
 
-        // Variable to store the maximum product found
-        int maxProd = 0;
-
-        // Loop over all starting positions of substrings
-        for (int i = 0; i < s.length(); i++) {
-            int prod = 1;
-
-            // Loop to extend the substring from position i to j
-            for (int j = i; j < s.length(); j++) {
-                // Convert the current character to a digit
-                int digit = Character.getNumericValue(s.charAt(j));
-
-                // Multiply the product by the current digit
-                prod *= digit;
-
-                // Update maxProd if the current product is greater
-                maxProd = Math.max(maxProd, prod);
+        for (int i = 0; i < len; i++) {
+            int digit1 = numStr.charAt(i) - '0';
+            for (int j = i + 1; j < len; j++) {
+                int digit2 = numStr.charAt(j) - '0';
+                int product = digit1 * digit2;
+                maxProduct = Math.max(maxProduct, product);
             }
         }
 
-        // Return the maximum digit product found among all substrings
-        return maxProd;
+        return maxProduct;
     }
 }
