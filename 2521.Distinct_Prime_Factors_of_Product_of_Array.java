a49e57e286512b687dadf280be1e365429a6502f
ermadhav <madhavtiwari2024@gmail.com>
2025-07-24 20:31:50 +0530

    Solved

--- 2521.Distinct_Prime_Factors_of_Product_of_Array.java
+++ 2521.Distinct_Prime_Factors_of_Product_of_Array.java
@@ -1,14 +1,21 @@
 class Solution {
     public int distinctPrimeFactors(int[] nums) {
-        Set<Integer> factors = new HashSet<>();
-        for(int num : nums){
-            for(int i=2; i<= num;i++){
-                while(num%i==0){
+        // Function to count distinct prime factors in the product of nums
+    public static int countDistinctPrimeFactors(int[] nums) {
+        Set<Integer> factors = new HashSet<>();  // To store unique prime factors
+        
+        // For each number in the array
+        for (int num : nums) {
+            // Check possible factors from 2 up to num
+            for (int i = 2; i <= num; i++) {
+                // While i divides num, add i to set and divide num by i
+                while (num % i == 0) {
                     factors.add(i);
                     num /= i;
                 }
             }
         }
-        return factors.size();
+        
+        return factors.size();  // Return count of distinct prime factors
     }
 }
