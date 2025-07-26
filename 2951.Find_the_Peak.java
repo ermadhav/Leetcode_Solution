17c51609969da414b010bddecd57f9950a628fae
ermadhav <madhavtiwari2024@gmail.com>
2025-07-26 14:42:08 +0530

    Solved

--- 2951.Find_the_Peak.java
+++ 2951.Find_the_Peak.java
@@ -1,11 +1,18 @@
 class Solution {
     public List<Integer> findPeaks(int[] mountain) {
+        // Create a list to store the indices of peak elements
         List<Integer> list = new ArrayList<>();
-        for(int i =1; i<mountain.length-1; i++){
-            if(mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]){
+
+        // Iterate through the array from the second element to the second last
+        for(int i = 1; i < mountain.length - 1; i++) {
+            // Check if the current element is greater than both its neighbors
+            if(mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]) {
+                // If it's a peak, add its index to the list
                 list.add(i);
             }
         }
+
+        // Return the list of peak indices
         return list;
     }
 }
