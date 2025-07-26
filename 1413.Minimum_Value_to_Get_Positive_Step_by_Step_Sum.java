78fcf5c6a2080b95c782e0d3e1aff4b9c3ba4a5a
ermadhav <madhavtiwari2024@gmail.com>
2025-07-26 23:23:26 +0530

    Solved

--- 1413.Minimum_Value_to_Get_Positive_Step_by_Step_Sum.java
+++ 1413.Minimum_Value_to_Get_Positive_Step_by_Step_Sum.java
@@ -1,13 +1,13 @@
 class Solution {
     public int minStartValue(int[] nums) {
-        int sum = 0;      // Running sum
-    int minSum = 0;   // Minimum running sum
-    
-    for (int num : nums) {
-        sum += num;               // Update sum
-        minSum = Math.min(minSum, sum);  // Track min sum
-    }
-    
-    return 1 - minSum;  // Minimum startValue to keep sum ≥ 1
+        int sum = 0; // Running sum
+        int minSum = 0; // Minimum running sum
+
+        for (int num : nums) {
+            sum += num; // Update sum
+            minSum = Math.min(minSum, sum); // Track min sum
+        }
+
+        return 1 - minSum; // Minimum startValue to keep sum ≥ 1
     }
 }
