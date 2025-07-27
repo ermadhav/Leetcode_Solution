b91b9ac3ba6cc382b812ad184133ec6192a66bbc
ermadhav <madhavtiwari2024@gmail.com>
2025-07-28 00:54:44 +0530

    Solvved

--- 3432.Count_Partitions_with_Even_Sum_Difference.java
+++ 3432.Count_Partitions_with_Even_Sum_Difference.java
@@ -1,12 +1,21 @@
 class Solution {
     public int countPartitions(int[] nums) {
         int total = 0, left = 0, count = 0;
-        for (int num : nums) total += num;
+
+        // Calculate total sum of the array
+        for (int num : nums)
+            total += num;
+
+        // Loop through possible split points (excluding last index to keep right
+        // non-empty)
         for (int i = 0; i < nums.length - 1; i++) {
-            left += nums[i];
-            if ((2 * left - total) % 2 == 0) count++;
+            left += nums[i]; // Add current element to left sum
+
+            // Check if the difference between left and right sums is even
+            if ((2 * left - total) % 2 == 0)
+                count++;
         }
 
-        return count;
+        return count; // Return the number of valid partitions
     }
 }
