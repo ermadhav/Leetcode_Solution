28f78eb360af1e5cb9e6dcf836174051856fca3a
ermadhav <madhavtiwari2024@gmail.com>
2025-07-27 23:16:54 +0530

    Solvved

--- 400.Nth_Digit.java
+++ 400.Nth_Digit.java
@@ -1,14 +1,21 @@
 class Solution {
     public int findNthDigit(int n) {
-        int len =1;
-        long count =9, start =1;
-        while(n > len*count){
-            n -= len * count;
-            len++;
-            count *= 10;
-            start *= 10;
+        int len = 1; // Current digit length (1 for 1-9, 2 for 10-99, etc.)
+        long count = 9; // Number of integers with 'len' digits
+        long start = 1; // Starting number with current digit length
+
+        // Determine the digit length group where the nth digit lies
+        while (n > len * count) {
+            n -= len * count; // Move past this group
+            len++; // Go to the next digit length
+            count *= 10; // 10 times more numbers in next group
+            start *= 10; // First number of the next group
         }
-        long num = start + (n-1)/len;
-        return String.valueOf(num).charAt((n-1)% len)-'0';
+
+        // Find the actual number that contains the nth digit
+        long num = start + (n - 1) / len;
+
+        // Find the digit within that number
+        return String.valueOf(num).charAt((n - 1) % len) - '0';
     }
 }
