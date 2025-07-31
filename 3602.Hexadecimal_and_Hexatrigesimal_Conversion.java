c86da3df85df6ece9a72cef587eab07404f85ac5
ermadhav <madhavtiwari2024@gmail.com>
2025-07-31 13:00:24 +0530

    Solved

--- 3602.Hexadecimal_and_Hexatrigesimal_Conversion.java
+++ 3602.Hexadecimal_and_Hexatrigesimal_Conversion.java
@@ -1,3 +1,11 @@
-public class 3602.Hexadecimal_and_Hexatrigesimal_Conversion {
-    
+class Solution {
+    public String concatHex36(int n) {
+        int square = n * n;           // Square of n
+        int cube = n * n * n;         // Cube of n
+        
+        String hex = Integer.toHexString(square).toUpperCase();  // Square in Hex
+        String base36 = Integer.toString(cube, 36).toUpperCase(); // Cube in Base36
+        
+        return hex + base36;          // Concatenate and return
+    }
 }
