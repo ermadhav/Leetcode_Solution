49627c342335253a2b955a5ecfceb4b42f0b692e
ermadhav <madhavtiwari2024@gmail.com>
2025-07-31 13:19:48 +0530

    Solved

--- 3079.Find_the_Sum_of_Encrypted_Integers.java
+++ 3079.Find_the_Sum_of_Encrypted_Integers.java
@@ -1,12 +1,22 @@
 class Solution {
     public int sumOfEncryptedInt(int[] nums) {
-        int total = 0;
+        int total = 0;  // Initialize sum of encrypted numbers
+
         for (int num : nums) {
+            // Convert the number to string so we can check its digits
             String s = String.valueOf(num);
+
+            // Find the largest digit (as char) using stream on the string's chars
             char maxDigit = (char) s.chars().max().getAsInt();
-            int encrypted = Integer.parseInt(String.valueOf((char) maxDigit).repeat(s.length()));
+
+            // Build the encrypted number by repeating the largest digit 
+            // for the length of the original number
+            int encrypted = Integer.parseInt(String.valueOf(maxDigit).repeat(s.length()));
+
+            // Add encrypted value to the running total
             total += encrypted;
         }
-        return total;
+
+        return total; // Return the sum of all encrypted numbers
     }
 }
