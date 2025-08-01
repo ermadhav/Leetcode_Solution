b3f5ab5413a096fdce2533641aa584afec70c58a
ermadhav <madhavtiwari2024@gmail.com>
2025-08-01 14:33:27 +0530

    Solved

--- 593.Valid_Square.java
+++ 593.Valid_Square.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
         int[][] pts = {p1, p2, p3, p4};
