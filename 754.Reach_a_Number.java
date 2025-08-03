5e791ce8cca1bec693dc64d7c1ef7f672ef3698e
ermadhav <madhavtiwari2024@gmail.com>
2025-08-03 09:38:30 +0530

    Solved

--- 754.Reach_a_Number.java
+++ 754.Reach_a_Number.java
@@ -1,9 +1,12 @@
 class Solution {
     public int reachNumber(int target) {
-        target = Math.abs(target);
+        target = Math.abs(target); // same for + or -
         int sum = 0, steps = 0;
-        while (sum < target || (sum - target) % 2 != 0)
+
+        // move until sum >= target and difference is even
+        while (sum < target || (sum - target) % 2 != 0) 
             sum += ++steps;
-        return steps;
+
+        return steps; // min moves
     }
 }
