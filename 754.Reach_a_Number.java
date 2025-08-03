37f8ccc2ece2d8b8d79057900fa63b5dfa768bdb
ermadhav <madhavtiwari2024@gmail.com>
2025-08-03 09:38:19 +0530

    Solved

---
+++ 754.Reach_a_Number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int reachNumber(int target) {
+        target = Math.abs(target);
+        int sum = 0, steps = 0;
+        while (sum < target || (sum - target) % 2 != 0)
+            sum += ++steps;
+        return steps;
+    }
+}
