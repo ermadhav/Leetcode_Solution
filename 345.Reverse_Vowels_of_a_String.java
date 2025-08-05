a75cd1b75c6e6c4f11ceab033deab4509992377f
ermadhav <madhavtiwari2024@gmail.com>
2025-08-05 14:16:12 +0530

    Solved

--- 345.Reverse_Vowels_of_a_String.java
+++ 345.Reverse_Vowels_of_a_String.java
@@ -1,16 +1,28 @@
 class Solution {
     public String reverseVowels(String s) {
+        // Convert string to char array for easy swapping
         char[] arr = s.toCharArray();
+        
+        // String containing all vowels (both uppercase and lowercase)
         String vowels = "aeiouAEIOU";
-        int i = 0, j = arr.length - 1;
+        
+        int i = 0, j = arr.length - 1; // Two pointers: start and end
 
+        // Continue until the two pointers meet
         while (i < j) {
+            // Move i forward until it finds a vowel
             while (i < j && vowels.indexOf(arr[i]) == -1) i++;
+            
+            // Move j backward until it finds a vowel
             while (i < j && vowels.indexOf(arr[j]) == -1) j--;
+            
+            // Swap the vowels at i and j
             char temp = arr[i];
             arr[i++] = arr[j];
             arr[j--] = temp;
         }
+        
+        // Return the new string after reversing vowels
         return new String(arr);
     }
 }
