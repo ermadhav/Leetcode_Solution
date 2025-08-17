cdc673a901b5de6b1687faeea2544a53ddd64c5d
ermadhav <madhavtiwari2024@gmail.com>
2025-08-17 19:04:37 +0530

    Solved

--- 409.Longest_Palindrome.java
+++ 409.Longest_Palindrome.java
@@ -1,3 +1,11 @@
-public class 409.Longest_Palindrome {
-    
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] count = new int[128]; // frequency array
+        for (char c : s.toCharArray()) count[c]++; 
+        
+        int length = 0;
+        for (int c : count) length += (c / 2) * 2; // add pairs
+        
+        return length < s.length() ? length + 1 : length; // add center if possible
+    }
 }
