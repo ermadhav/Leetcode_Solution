1325cd0fe7a27edae15ba698baa566963f214a2b
ermadhav <madhavtiwari2024@gmail.com>
2025-09-10 21:23:53 +0530

    Solved

--- 2578.Split_With_Minimum_Sum.java
+++ 2578.Split_With_Minimum_Sum.java
@@ -1,3 +1,17 @@
-public class 2578.Split_With_Minimum_Sum {
-    
+import java.util.*;
+
+class Solution {
+    public int splitNum(int num) {
+        // Convert number to digits and sort
+        char[] d = String.valueOf(num).toCharArray();
+        Arrays.sort(d);
+
+        // Build two numbers alternately
+        StringBuilder a = new StringBuilder(), b = new StringBuilder();
+        for (int i = 0; i < d.length; i++)
+            if (i % 2 == 0) a.append(d[i]); else b.append(d[i]);
+
+        // Return sum of both numbers
+        return Integer.parseInt(a.toString()) + Integer.parseInt(b.toString());
+    }
 }
