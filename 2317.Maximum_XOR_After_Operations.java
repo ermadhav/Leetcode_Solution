ab893f9c82bf5a2452ded2cf722c99e39efac3fd
ermadhav <madhavtiwari2024@gmail.com>
2025-09-18 21:59:52 +0530

    Solved

--- 2317.Maximum_XOR_After_Operations.java
+++ 2317.Maximum_XOR_After_Operations.java
@@ -2,7 +2,7 @@ class Solution {
     public int maximumXOR(int[] nums) {
         int result = 0;
         for (int num : nums) {
-            result |= num;
+            result |= num; // bitwise OR accumulates all available bits
         }
         return result;
     }
