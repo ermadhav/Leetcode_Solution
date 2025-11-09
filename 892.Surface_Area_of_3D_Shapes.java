d585dd413b1bdb0f95a219e4691bad734184a6b2
ermadhav <madhavtiwari2024@gmail.com>
2025-11-09 18:08:32 +0530

    Solved

--- 892.Surface_Area_of_3D_Shapes.java
+++ 892.Surface_Area_of_3D_Shapes.java
@@ -1,3 +1,29 @@
-public class 892.Surface_Area_of_3D_Shapes {
-    
+class Solution {
+    public int surfaceArea(int[][] grid) {
+        int n = grid.length;
+        int area = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int h = grid[i][j];
+
+                // Add surface from this tower
+                if (h > 0) {
+                    area += 6 * h - 2 * (h - 1); // remove hidden faces inside the tower
+                }
+
+                // Remove hidden faces with right neighbor
+                if (j + 1 < n) {
+                    area -= 2 * Math.min(h, grid[i][j + 1]);
+                }
+
+                // Remove hidden faces with bottom neighbor
+                if (i + 1 < n) {
+                    area -= 2 * Math.min(h, grid[i + 1][j]);
+                }
+            }
+        }
+
+        return area;
+    }
 }
