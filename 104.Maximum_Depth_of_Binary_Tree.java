d8e26c3167a10a87bd5f3ed90bcec0f22fcbea9c
ermadhav <madhavtiwari2024@gmail.com>
2025-11-14 21:12:57 +0530

    Solved

--- 104.Maximum_Depth_of_Binary_Tree.java
+++ 104.Maximum_Depth_of_Binary_Tree.java
@@ -1,3 +1,6 @@
-public class 104.Maximum_Depth_of_Binary_Tree {
-    
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0; // if tree is empty, depth is 0
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right)); // depth = 1 + max depth of subtrees
+    }
 }
