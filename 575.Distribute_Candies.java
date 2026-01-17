cdb378d09bf933336c5cf475f300d07688360f39
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-01-17 21:07:47 +0530

    Solved

--- 575.Distribute_Candies.java
+++ 575.Distribute_Candies.java
@@ -1,3 +1,20 @@
-public class 575.Distribute_Candies {
-    
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        // Set to store unique candy types
+        Set<Integer> set = new HashSet<>();
+
+        // Add all candy types to the set
+        for (int x : candyType) {
+            set.add(x);
+        }
+
+        // Count of unique candy types
+        int unique = set.size();
+
+        // Sister can eat at most half of the candies
+        int ans = Math.min(unique, candyType.length / 2);
+
+        // Return the maximum number of different candies she can eat
+        return ans;
+    }
 }
