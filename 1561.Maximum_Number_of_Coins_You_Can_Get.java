43aa51a8c57a0d700124b6f393f9e92a87af1a4f
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-01-23 14:53:54 +0530

    Solved

--- 1561.Maximum_Number_of_Coins_You_Can_Get.java
+++ 1561.Maximum_Number_of_Coins_You_Can_Get.java
@@ -1,15 +1,23 @@
 class Solution {
     public int maxCoins(int[] piles) {
         Arrays.sort(piles);
-        int left =0;
-        int right = piles.length-1;
-        int mycoins =0;
-        while(left < right){
+
+        int left = 0;
+        int right = piles.length - 1;
+        int myCoins = 0;
+
+        while (left < right) {
+            // Alice takes the largest
             right--;
-            mycoins += piles[right];
+
+            // You take the second largest
+            myCoins += piles[right];
             right--;
+
+            // Bob takes the smallest
             left++;
         }
-        return mycoins;
+
+        return myCoins;
     }
 }
