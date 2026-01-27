049d6a3b9b9f320c14949fbe20a0aa32ca7ce9bd
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-01-27 23:14:01 +0530

    Solved

---
+++ 2582.Pass_the_Pillow.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+        int cycle = 2 * (n - 1);
+        int t = time % cycle;
+
+        if (t < n) {
+            return 1 + t;   // moving forward
+        } else {
+            return n - (t - (n - 1)); // moving backward
+        }
+    }
+}
