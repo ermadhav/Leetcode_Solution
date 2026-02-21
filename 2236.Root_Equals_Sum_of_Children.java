24bd5dc909e09d247d3baff9bc408b5f0d2124ea
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-02-21 10:39:27 +0530

    Solved

--- 2236.Root_Equals_Sum_of_Children.java
+++ 2236.Root_Equals_Sum_of_Children.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean checkTree(TreeNode root) {
-        // Ye to Aana Hi chahiye 
+        // Leetcode Prob No:- 2236
+        // Ye to Aana Hi chahiye
         if(root.val == root.right.val + root.left.val) return true;
         return false;
     }
