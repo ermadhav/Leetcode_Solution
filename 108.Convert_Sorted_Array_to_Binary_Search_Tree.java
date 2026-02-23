81f2c43e0d77c5ec737817aa4425458b33abb4bb
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-02-23 19:27:55 +0530

    Solved

---
+++ 108.Convert_Sorted_Array_to_Binary_Search_Tree.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return buildBST(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildBST(int[] nums, int left, int right) {
+
+        // Base case
+        if (left > right) {
+            return null;
+        }
+
+        // Choose middle element
+        int mid = left + (right - left) / 2;
+
+        // Create root node
+        TreeNode root = new TreeNode(nums[mid]);
+
+        // Recursively build left and right subtrees
+        root.left = buildBST(nums, left, mid - 1);
+        root.right = buildBST(nums, mid + 1, right);
+
+        return root;
+    }
+}
