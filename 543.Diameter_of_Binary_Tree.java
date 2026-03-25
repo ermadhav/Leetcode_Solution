450015b7e0a7edc569aeb4f6f29851d78284dcb7
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-03-25 11:50:48 +0530

    Solved

--- 543.Diameter_of_Binary_Tree.java
+++ 543.Diameter_of_Binary_Tree.java
@@ -1,19 +1,16 @@
 class Solution {
-
-    private int diameter = 0;   // <-- declare diameter
-
     public int diameterOfBinaryTree(TreeNode root) {
-        depth(root);
-        return diameter;
+        int[] diameter = new int[1];
+        height(root, diameter);
+        return diameter[0];
     }
-
-    private int depth(TreeNode node) {   // <-- fix class name
-        if (node == null) return 0;
-
-        int left = depth(node.left);
-        int right = depth(node.right);
-
-        diameter = Math.max(diameter, left + right); // update diameter
-        return Math.max(left, right) + 1;
+    static int height(TreeNode node, int[] diameter){
+        if(node == null){
+            return 0;
+        }
+        int lh = height(node.left, diameter);
+        int rh = height(node.right, diameter);
+        diameter[0] = Math.max(diameter[0], lh+rh);
+        return 1+Math.max(lh, rh);
     }
 }
