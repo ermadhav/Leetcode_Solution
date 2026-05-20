a63b355bc17afeddd642a2e064798304c6b6f63c
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-05-20 14:08:14 +0530

    solved

--- 274.H-Index.java
+++ 274.H-Index.java
@@ -1,4 +1,15 @@
-public ed"
- {
-    
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int count = 0;
+        int n = citations.length;
+
+        for(int i = 0; i < n; i++){
+            if(citations[i] >= n - i){
+                count = n - i;
+                break;
+            }
+        }
+        return count;
+    }
 }
