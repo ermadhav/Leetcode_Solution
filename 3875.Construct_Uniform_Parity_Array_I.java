82d5fe41acde91f8e465651881374231645420ed
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-05-24 21:58:53 +0530

    Solved

--- 3875.Construct_Uniform_Parity_Array_I.java
+++ 3875.Construct_Uniform_Parity_Array_I.java
@@ -1,16 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        if(nums1.length <= 2){
-            return true;
-        }
-        
-        Arrays.sort(nums1);
-        int diff = nums1[1]- nums1[0];
-        for(int i=2; i<nums1.length; i++){
-            if(nums1[i] - nums1[i-1] != diff){
-                return false;
-            }
-        }
         return true;
     }
 }
