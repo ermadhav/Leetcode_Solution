a2efd5716e3fbdb1950055d9c7dd7b4c3c6fd1fd
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-07 23:01:06 +0530

    Solved

---
+++ 3668.Restore_Finishing_Order.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] recoverOrder(int[] order, int[] friends) {
+
+        // Friends ko HashSet me store kar do
+        HashSet<Integer> set = new HashSet<>();
+        for (int friend : friends) {
+            set.add(friend);
+        }
+
+        // Answer store karne ke liye array
+        int[] ans = new int[friends.length];
+        int index = 0;
+
+        // Race ke order ko ek-ek karke dekho
+        for (int racer : order) {
+
+            // Agar racer friend hai
+            if (set.contains(racer)) {
+
+                // Answer me add kar do
+                ans[index] = racer;
+                index++;
+            }
+        }
+
+        // Final answer return karo
+        return ans;
+    }
+}
