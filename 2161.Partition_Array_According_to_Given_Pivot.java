6c87e1706787a40df3110d241eb9f20dd7340b4d
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-09 00:16:17 +0530

    Solved

--- 2161.Partition_Array_According_to_Given_Pivot.java
+++ 2161.Partition_Array_According_to_Given_Pivot.java
@@ -4,6 +4,7 @@ class Solution {
         ArrayList<Integer> a2 = new ArrayList<>();
         ArrayList<Integer> a3 = new ArrayList<>();
         ArrayList<Integer> ans = new ArrayList<>();
+        
          // Array ko 3 parts mein divide karo
         for(int i=0; i<nums.length; i++){
             if(nums[i]<pivot){
