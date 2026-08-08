9008d470812a2fd924bd03846da107c81929bef1
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-09 00:13:32 +0530

    Solved

---
+++ 2161.Partition_Array_According_to_Given_Pivot.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        ArrayList<Integer> a1 = new ArrayList<>();
+        ArrayList<Integer> a2 = new ArrayList<>();
+        ArrayList<Integer> a3 = new ArrayList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]<pivot){
+                a1.add(nums[i]);
+            }else if(nums[i] == pivot){
+                a2.add(nums[i]);
+            }else{
+                a3.add(nums[i]);
+            }
+        }
+        ans.addAll(a1);
+        ans.addAll(a2);
+        ans.addAll(a3);
+
+        int[] result = new int[ans.size()];
+        for(int i=0; i<ans.size(); i++){
+            result[i] = ans.get(i);
+        }
+        return result;
+    }
+}
