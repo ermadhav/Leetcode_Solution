108949c0f643ee21a582b3bdad289952f82f1e9f
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-09 21:51:50 +0530

    Solved

---
+++ 3838.Weighted_Word_Mapping.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        
+        StringBuilder ans = new StringBuilder();
+
+        for(String word : words){
+            int sum =0;
+            for(char ch : word.toCharArray()){
+
+                int index = ch - 'a';
+                sum += weights[index];
+
+            }
+
+            int value = sum % 26;
+            char mapped = (char)('a' + (25 - value));
+            ans.append(mapped);
+        }
+
+        return ans.toString();
+    }
+}
