d52973840740aeb34726503f3ffbe98b0dd7d5bb
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-10 22:39:21 +0530

    Solved

---
+++ 3731.Find_Missing_Elements.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++){
+            for(int j = nums[i]+1; j<nums[i+1]; j++) {
+                ans.add(j);
+            }
+        }
+        return ans;
+    }
+}
