4d1a4a73b62a8bae87b75a7884507f6484932f23
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-10 22:41:12 +0530

    Solved

--- 3731.Find_Missing_Elements.java
+++ 3731.Find_Missing_Elements.java
@@ -2,7 +2,11 @@ class Solution {
     public List<Integer> findMissingElements(int[] nums) {
         List<Integer> ans = new ArrayList<>();
         Arrays.sort(nums);
+
+        // Har consecutive element ke beech missing numbers ko check kar 
         for(int i=0; i<nums.length; i++){
+
+            // nums[i] aur nums[i+1] ke beech ke numbers ko add kar
             for(int j = nums[i]+1; j<nums[i+1]; j++) {
                 ans.add(j);
             }
