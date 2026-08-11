306d1fbc488003527d7b909235dabe9aa6054942
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-11 23:44:03 +0530

    Solved

--- 2610.Convert_an_Array_Into_a_2D_Array_With_Conditions.java
+++ 2610.Convert_an_Array_Into_a_2D_Array_With_Conditions.java
@@ -1,13 +1,21 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
+
+        // freq store karega
         int[] freq = new int[nums.length + 1];
 
         for (int num : nums) {
+
+            // curr num ki freq incrs 
             freq[num]++;
+
+            // create a new row if needed 
+            // ye bateyaga ki freq[num] konsi row me jayega 
             if(ans.size() < freq[num]){
                 ans.add(new ArrayList<>());
             }
+            
             ans.get(freq[num] - 1).add(num);
         }
         return ans;
