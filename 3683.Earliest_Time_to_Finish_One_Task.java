5c763a6de976e89ee298e1a5caf5b4d1ba28a6b9
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-12 23:53:17 +0530

    Solved

--- 3683.Earliest_Time_to_Finish_One_Task.java
+++ 3683.Earliest_Time_to_Finish_One_Task.java
@@ -1,9 +1,16 @@
 class Solution {
     public int earliestTime(int[][] tasks) {
-        int min = Integer.MAX.VALUE;
 
+        // Store the minimum finishing time
+        int min = Integer.MAX_VALUE;
+
+        // Check each task
         for(int[] task:tasks){
+
+            // Finish time = start time + duration
             int finishTime = task[0]+task[1];
+
+            // Update minimum finish time
             min = Math.min(min, finishTime);
         }
         return min;
