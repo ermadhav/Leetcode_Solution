d003ef6de9d98c46708ab4788e4f381817d5510d
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-13 02:53:22 +0530

    Solved

--- 450_DSA/Array/Reverse_the_array.java
+++ 450_DSA/Array/Reverse_the_array.java
@@ -3,7 +3,8 @@ class Solution {
         int n = arr.length;
         int[] ans = new int[n];
         for(int i=0; i<n; i++){
-            ans[i] = arr[n-i-1];
+
+            ans[i] = arr[n-i-1]; // arr[n-i-1] is used to get the elements from last
         }
         for(int i=0; i<n;i++){
             arr[i] = ans[i];
