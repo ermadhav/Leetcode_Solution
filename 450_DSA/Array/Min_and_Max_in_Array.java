2c08196ad88afe8daff1aeb500e325bd4f137b5e
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-13 03:09:31 +0530

    Solved

--- 450_DSA/Array/Min_and_Max_in_Array.java
+++ 450_DSA/Array/Min_and_Max_in_Array.java
@@ -1,13 +1,15 @@
 class Solution {
     public ArrayList<Integer> getMinMax(int[] arr) {
-        int min = arr[0];
+        // this two will be used to compare the min and max
+        int min = arr[0]; 
         int max = arr[0];
         ArrayList<Integer> ans = new ArrayList<>();
         
         for(int i=0; i<arr.length; i++){
-            if(arr[i] < min) min = arr[i];
-            if(arr[i] > max) max = arr[i];
+            if(arr[i] < min) min = arr[i]; // new min found update it
+            if(arr[i] > max) max = arr[i]; // new max found update it
         }
+
         ans.add(min);
         ans.add(max);
         return ans;
