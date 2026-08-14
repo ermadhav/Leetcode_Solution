f17588b78edc7c5cf8aa41abbef352f3764cd2c8
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-15 04:21:02 +0530

    Solved

---
+++ 450_DSA/Array/Kth_Smallest.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int kthSmallest(int[] arr, int k) {
+
+        // sort karke kth-1(0th index that's why) element return karde
+        Arrays.sort(arr);
+        return arr[k-1];
+    }
+}
