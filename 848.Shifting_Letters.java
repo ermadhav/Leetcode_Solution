dec2e221caf59117a2d90b6c6352c278a75669a6
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-16 00:49:48 +0530

    Solved

--- 848.Shifting_Letters.java
+++ 848.Shifting_Letters.java
@@ -1,8 +1,11 @@
 class Solution {
     public String shiftingLetters(String s, int[] shifts) {
         char[] arr = s.toCharArray();
+
+        // stores the total shift of curr char
         int shift = 0;
 
+        // piche se aage traverse 
         for(int i = shifts.length-1; i>= 0; i--){
             shift =  (shift + shifts[i])%26;
             arr[i] = (char) ((arr[i] - 'a' + shift) % 26 + 'a');
