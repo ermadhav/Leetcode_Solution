76772179ee56579add8a31693b9ea4dd36b37c7c
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-16 01:33:58 +0530

    Solved

--- 450_DSA/Array/Move_all_negative_elements_to_end.java
+++ 450_DSA/Array/Move_all_negative_elements_to_end.java
@@ -1,13 +1,19 @@
 class Solution {
 	public void segregateElements(int[] arr) {
+
+        // creating same size array 
 		int[] temp = new int[arr.length];
 		int k = 0;
+
+        // firstly storing the +ves and 0s
 		for (int i = 0; i<arr.length; i++) {
 			if (arr[i] >= 0) {
 				temp[k] = arr[i];
 				k++;
 			}
 		}
+
+        // and then storing the -ves 
 		for (int j = 0; j<arr.length; j++) {
 			if (arr[j] < 0) {
 				temp[k] = arr[j];
