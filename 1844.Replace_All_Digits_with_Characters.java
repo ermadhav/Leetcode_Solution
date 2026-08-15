75f10cfa46042e1798d7bfa8560a1fea26037d01
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-15 22:04:04 +0530

    Solved

--- 1844.Replace_All_Digits_with_Characters.java
+++ 1844.Replace_All_Digits_with_Characters.java
@@ -2,8 +2,13 @@ class Solution {
     public String replaceDigits(String s) {
         char[] arr = s.toCharArray();
 
+        // odd index per ditis hai
         for(int i=1; i<arr.length; i+=2){
+
+            // char --> int conversion
             int digit = arr[i] - '0';
+
+            // eg a+1 = b
             arr[i] = (char)(arr[i-1]+digit);
         }
         return new String(arr);
