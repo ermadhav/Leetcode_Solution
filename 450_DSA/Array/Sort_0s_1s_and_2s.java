040c1135c6f057fb10b155775977096c6b170787
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-16 02:44:49 +0530

    Solved

--- 450_DSA/Array/Sort_0s_1s_and_2s.java
+++ 450_DSA/Array/Sort_0s_1s_and_2s.java
@@ -1,7 +1,11 @@
 void sort012(int arr[], int n) {
 	for (int i = 0; i<n; i++) {
 		for (int j = 0; j<n - 1; j++) {
+
+			// left element bigger , swap kar
 			if (arr[j] > arr[j + 1]) {
+
+				// swapping 
 				int temp = arr[j];
 				arr[j] = arr[j + 1];
 				arr[j + 1] = temp;
