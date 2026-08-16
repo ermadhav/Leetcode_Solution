d0208c7ab984ab0ebc7deec87e81881e90d84156
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-16 15:42:40 +0530

    Solved

---
+++ 1347.Minimum_Number_of_Steps_to_Make_Two_Strings_Anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minSteps(String s, String t) {
+        int[] freq = new int[26];
+
+        // count the char of s
+        for(char ch: s.toCharArray()){
+            freq[ch - 'a']++;
+        }
+
+        // count the char of s  
+        for(char ch: t.toCharArray()){
+            freq[ch - 'a']--;
+        }
+
+
+        int steps = 0;
+
+        // T ke missing character ko count karna 
+        for(int count:freq){
+            if(count > 0){
+                steps += count;
+            }
+        }
+        return steps;
+    }
+}
