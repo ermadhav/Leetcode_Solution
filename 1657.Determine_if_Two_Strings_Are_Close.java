4efe7f801f9cd6cd150a3af6fe05376202a89a59
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-19 23:29:07 +0530

    Solved

--- 1657.Determine_if_Two_Strings_Are_Close.java
+++ 1657.Determine_if_Two_Strings_Are_Close.java
@@ -5,23 +5,34 @@ class Solution {
         if(word1.length() != word2.length()){
             return false;
         }
+        // Store frequency of each char in both strings
         int[] freq1 = new int[26];
         int[] freq2 = new int[26];
 
+        // count char of word1
         for(char ch: word1.toCharArray()){
             freq1[ch - 'a']++;
         }
+
+        // count char of word2
         for(char ch: word2.toCharArray()){
             freq2[ch - 'a']++;
         }
 
+        // Dono String me same set of char hone chahiye
+        // agar char ek string me hai but dusre me nahi to, return false
         for(int i=0; i<26; i++){
             if((freq1[i] == 0) != (freq2[i] == 0)){
                 return false;
             }
         }
+
+        // freq sort taki   patter ko compare kar sake
         Arrays.sort(freq1);
+        
         Arrays.sort(freq2);
+
+
         return Arrays.equals(freq1, freq2);
     }
 }
