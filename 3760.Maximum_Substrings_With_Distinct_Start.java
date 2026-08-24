fdb78afb17c77739343ba50955e8ff8c6f25c65f
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-24 21:56:59 +0530

    Solved

--- 3760.Maximum_Substrings_With_Distinct_Start.java
+++ 3760.Maximum_Substrings_With_Distinct_Start.java
@@ -1,10 +1,20 @@
 class Solution {
     public int maxDistinct(String s) {
         boolean[] seen = new boolean[26];
+
+        // no. of distinct char
         int count = 0;
+
+        //
         for(char c: s.toCharArray()){
+
+            // if not seen before
             if(!seen[c-'a']){
+
+                // mark seen
                 seen[c-'a'] = true;
+
+                // 
                 count++;
             }
         }
