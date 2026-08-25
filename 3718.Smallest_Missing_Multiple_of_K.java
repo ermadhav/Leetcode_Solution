a903c5b0dc4318e085209fedb0a190832d37df40
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-25 19:25:29 +0530

    Solved

--- 3718.Smallest_Missing_Multiple_of_K.java
+++ 3718.Smallest_Missing_Multiple_of_K.java
@@ -1,7 +1,8 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
         HashSet<Integer> set = new HashSet<>();
-
+        
+        // Array ke saare elements ko HashSet mein add kar rahe hain
         for(int i=0; i<nums.length; i++){
             set.add(nums[i]);
         }
