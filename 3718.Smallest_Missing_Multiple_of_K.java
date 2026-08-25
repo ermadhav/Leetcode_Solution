b235ff6aa69b6d31db7425b1a17580c150bfa25c
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-25 19:26:06 +0530

    Solved

--- 3718.Smallest_Missing_Multiple_of_K.java
+++ 3718.Smallest_Missing_Multiple_of_K.java
@@ -9,9 +9,14 @@ class Solution {
 
         // Sabse pehla multiple k se start hoga
         int multiple = k;
+
+        // Jab tak current multiple set mein present hai,
+        // tab tak next multiple par move karte rahenge
         while(set.contains(multiple)){
             multiple += k ;
         }
+
+        // Jo multiple set mein nahi mila, wahi answer hai
         return multiple;
     }
 }
