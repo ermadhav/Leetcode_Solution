01deff69c52e13aadd72dfc91180dcf2788e6dfa
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-26 22:40:24 +0530

    Solved

--- 1221.Split_a_String_in_Balanced_Strings.java
+++ 1221.Split_a_String_in_Balanced_Strings.java
@@ -3,9 +3,11 @@ class Solution {
         int balance = 0; // l or r ka balnc track karega
         int count = 0; // kitne balance vo 
         for(char c: s.toCharArray()){
+            // L mila to +1
             if(c == 'L'){
                 balance++;
             }else{
+                // L mila to -1
                 balance--;
             }
             // jab l or r == ho to 1 substring mil gayi
