713d3cb3ee79f721aecc7b071a4bbbac7ae07639
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-28 22:27:07 +0530

    Solved

--- 2294.Partition_Array_Such_That_Maximum_Difference_Is_K.java
+++ 2294.Partition_Array_Such_That_Maximum_Difference_Is_K.java
@@ -8,6 +8,8 @@ class Solution {
         // Current subsequence ka minimum element
         int start =nums[0];
 
+        // Agar difference k se zyada ho gaya,
+        // toh new subsequence banana padega
         for(int i=1; i<nums.length; i++){
             if(nums[i] - start>k){
                 count++;
