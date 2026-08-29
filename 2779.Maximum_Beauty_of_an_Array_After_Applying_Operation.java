a5498594280b5068f1f4e3ba13ee47132dc2e660
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-29 21:48:14 +0530

    Solved

--- 2779.Maximum_Beauty_of_an_Array_After_Applying_Operation.java
+++ 2779.Maximum_Beauty_of_an_Array_After_Applying_Operation.java
@@ -4,6 +4,8 @@ class Solution {
         int left =0;
         int ans =0;
 
+        // Agar current range ka difference 2*k se zyada hai, toh left pointer ko aage badhao
+
         for(int i=0; i<nums.length; i++){
             while(nums[i] - nums[left] > 2*k){
                 left++;
