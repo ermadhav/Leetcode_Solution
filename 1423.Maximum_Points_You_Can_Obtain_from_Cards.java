0bad62e5eaafde2cd1c7cc313f06a82316c36198
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-30 21:53:50 +0530

    Solved

---
+++ 1423.Maximum_Points_You_Can_Obtain_from_Cards.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int sum =0;
+        int subSum =0;
+        for(int i=0; i<cardPoints.length; i++){
+            sum += cardPoints[i];
+        }
+        for(int i=0; i<cardPoints.length-k; i++){
+            subSum += cardPoints[i];
+        }
+        int min = subSum;
+        for(int i = cardPoints.length-k; i<cardPoints.length; i++){
+            subSum += cardPoints[i];
+            subSum -= cardPoints[i- (cardPoints.length-k)];
+            min = Math.min(min, subSum);
+        }
+        return sum - min;
+    }
+}
