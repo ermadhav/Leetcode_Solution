07536757a812c3765542ac7e4e9d0270e08f3487
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-30 22:03:36 +0530

    Solved

--- 1423.Maximum_Points_You_Can_Obtain_from_Cards.java
+++ 1423.Maximum_Points_You_Can_Obtain_from_Cards.java
@@ -1,25 +1,28 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
-        int sum =0;
-        int subSum =0;
+        int sum = 0;
+        int subSum = 0;
 
         // Total sum of Array
-        for(int i=0; i<cardPoints.length; i++){
+        for (int i = 0; i < cardPoints.length; i++) {
             sum += cardPoints[i];
         }
 
         // N = cardPoints.length
         // Hume N-k cards ko remove/leave karna hai
         // Starting window ka sum calculate kar rahe hain
-        for(int i=0; i<cardPoints.length-k; i++){
+        for (int i = 0; i < cardPoints.length - k; i++) {
             subSum += cardPoints[i];
         }
 
         // Pehli window ko minimum maan rahe hain
         int min = subSum;
-        for(int i = cardPoints.length-k; i<cardPoints.length; i++){
+
+        // Sliding window use karke minimum sum find kar rahe hain
+
+        for (int i = cardPoints.length - k; i < cardPoints.length; i++) {
             subSum += cardPoints[i];
-            subSum -= cardPoints[i- (cardPoints.length-k)];
+            subSum -= cardPoints[i - (cardPoints.length - k)];
             min = Math.min(min, subSum);
         }
         return sum - min;
