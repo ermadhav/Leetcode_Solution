3ef8c9d11ca90d84898090d597702983a483145a
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-30 16:38:00 +0530

    Solved

--- 2091.Removing_Minimum_and_Maximum_From_Array.java
+++ 2091.Removing_Minimum_and_Maximum_From_Array.java
@@ -19,7 +19,7 @@ class Solution {
        int left = Math.min(min, max); 
        int right = Math.max(min, max);
 
-       // Dono ko front se remove karne ki deletions
+       // Dono ko front or back se remove karne ki deletions
        int front = right + 1;
        int back = nums.length - left;
        int eachSide = (left+1) + (nums.length-right);
