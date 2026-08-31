ca8095285f021c9c7cc86af0679236469c0f4577
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-31 22:14:19 +0530

    Solved

--- 950.Reveal_Cards_In_Increasing_Order.java
+++ 950.Reveal_Cards_In_Increasing_Order.java
@@ -6,9 +6,11 @@ class Solution {
         Queue<Integer> q = new LinkedList<>();
          // Cards ko increasing order mein sort karo
         Arrays.sort(deck);
+        // Queue mein saare indices add karo
         for(int i = 0; i < deck.length; i++){
             q.add(i);
         }
+        // Smallest card se start karke positions fill karo
         for(int card: deck){
             int index = q.poll();
             result[index] = card;
