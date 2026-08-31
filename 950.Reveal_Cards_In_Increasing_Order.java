bc076bb06df08f12491f647daf54dc808e784b74
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-08-31 22:02:59 +0530

    Solved

---
+++ 950.Reveal_Cards_In_Increasing_Order.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        int[] result = new int[deck.length];
+        Queue<Integer> q = new LinkedList<>();
+        Arrays.sort(deck);
+        for(int n : deck){
+            q.add(n);
+        }
+        for(int card: deck){
+            int index = q.poll();
+            ans[index] = card;
+            if(!q.isEmpty()){
+                q.add(q.poll());
+            }
+        }
+        return ans;
+    }
+}
