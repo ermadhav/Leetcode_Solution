5d871ce0d304aec9d7d99ef369917a49e4d773a1
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-01 20:49:47 +0530

    Solved

--- 2966.Divide_Array_Into_Arrays_With_Max_Difference.java
+++ 2966.Divide_Array_Into_Arrays_With_Max_Difference.java
@@ -1,7 +1,10 @@
 class Solution {
     public int[][] divideArray(int[] nums, int k) {
         Arrays.sort(nums);
+
+        // n/3 groups banenge aur har group mein 3 elements honge
         int[][] ans = new int[nums.length/3][3];
+        // Har baar 3 elements ko ek group mein lenge
         for(int i=0; i<nums.length; i +=3){
             if(nums[i+2]-nums[i]>k){
                 return new int[0][0];
