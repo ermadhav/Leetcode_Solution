4ff08d635ca5a66c2fcd6e0ba38b1a08b30f0a4f
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-02 17:57:21 +0530

    Solved

--- 2094.Finding_3_Digit_Even_Numbers.java
+++ 2094.Finding_3_Digit_Even_Numbers.java
@@ -6,8 +6,12 @@ class Solution {
 
         // will be used to avoid duplicate
         Set<Integer> set = new HashSet<>();
+
+        // first digit(100th place)
         for(int i=0; i<l; i++){
+            // second digit(10th place)
             for(int j=0; j<l; j++){
+                // third digit(1st place)
                 for(int k=0; k<l; k++){
                     if(i != j && j != k && i != k && digits[i] != 0 && digits[k]%2 == 0){
                         set.add(digits[i]*100 + digits[j]*10 + digits[k]);
