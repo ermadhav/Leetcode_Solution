3be89c9da9d1c3a6d0181f97c7680f04ab2530ce
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-02 18:13:14 +0530

    Solved

---
+++ 3483.Unique_3-Digit_Even_Numbers.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        int l = digits.length;
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<l; i++){
+            for(int j=0; j<l; j++){
+                for(int k=0; k<l; k++){
+                    if(i != j && j != k && i != k && digits[i] != 0 && digits[k]%2 == 0){
+                        set.add(digits[i]*100 + digits[j]*10 + digits[k]);
+                    }
+                }
+            }
+        }
+        return set.size();
+    }
+}
