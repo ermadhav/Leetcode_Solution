dee1b5b85b5216e29f883be3e72ca0ce3fc28518
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-02 18:42:11 +0530

    Solved

--- 1295.Find_Numbers_with_Even_Number_of_Digits.java
+++ 1295.Find_Numbers_with_Even_Number_of_Digits.java
@@ -2,12 +2,16 @@ class Solution {
     public int findNumbers(int[] nums) {
         
         int count =0; // stores cout of no. have even digits
-        
+
         for(int i=0; i<nums.length; i++){
+
+            // no. of digits in curr no.
             int digits =0;
             while(nums[i] != 0){
+
+                // remove the last digit
                 nums[i] = nums[i]/10;
-                digits++;
+                digits++; // incrs the count
             }
             if(digits%2 == 0){
                 count++;
