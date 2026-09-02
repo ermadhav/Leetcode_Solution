1bcde8c9f67cef228a5d581e99af111735f9b8dd
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-02 17:52:47 +0530

    Solved

---
+++ 2094.Finding_3_Digit_Even_Numbers.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] findEvenNumbers(int[] digits) {
+        int l =  digits.length;
+        int index = 0;
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<l; i++){
+            for(int j=0; j<l; j++){
+                for(int k=0; k<l; k++){
+                    if(i != j && j != k && i != k && digits[i] != 0 && digits[k]%2 == 0){
+                        set.add(digits[i]*100 + digits[j]*10 + digits[k]);
+                    }
+                }
+            }
+        }
+        int arr[] = new int[set.size()];
+        for(int n: set){
+            arr[index++] = n;
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+}
