667d3cfb996a4b636d8f7831f41fe6e743358282
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-02 17:59:14 +0530

    Solved

--- 2094.Finding_3_Digit_Even_Numbers.java
+++ 2094.Finding_3_Digit_Even_Numbers.java
@@ -13,16 +13,25 @@ class Solution {
             for(int j=0; j<l; j++){
                 // third digit(1st place)
                 for(int k=0; k<l; k++){
+
+                    // All 3 posn is diff, not a leading zero, last digit is even
                     if(i != j && j != k && i != k && digits[i] != 0 && digits[k]%2 == 0){
+                        // create a three digit no.
                         set.add(digits[i]*100 + digits[j]*10 + digits[k]);
                     }
                 }
             }
         }
+
+        // arr with set size
         int arr[] = new int[set.size()];
+
+        // copy set no. into arr
         for(int n: set){
             arr[index++] = n;
         }
+
+        // sort (req)
         Arrays.sort(arr);
         return arr;
     }
