36ac51bfca56926b0a0b526c8c2617b489ea61d4
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-02 22:52:48 +0530

    Solved

---
+++ 3074.Apple_Redistribution_into_Boxes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        Arrays.sort(capacity);
+
+        int num =0;
+        for(int i=0; i<apple.length; i++){
+            num += apple[i];
+        }
+
+        int cap =0;
+        int count =0;
+
+        for(int i= capacity.length-1; i>=0; i--){
+            cap += capacity[i];
+            count++;
+            if(cap >= num){
+                return count;
+            }
+        }
+        return count;
+    }
+}
