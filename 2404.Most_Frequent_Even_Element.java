7bbccbcfba59031f766ff8bcb060675dfb12a8ed
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-04 05:25:30 +0530

    Solved

--- 2404.Most_Frequent_Even_Element.java
+++ 2404.Most_Frequent_Even_Element.java
@@ -2,15 +2,18 @@ class Solution {
     public int mostFrequentEven(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
+        // count freq of even no.
         for(int n:nums){
             if(n%2 == 0){
                 map.put(n, map.getOrDefault(n, 0)+1);
             }
         }
 
+
         int maxFreq = 0;
         int ans = -1;
 
+        // finding the most freq even no.
         for(Map.Entry<Integer,Integer> entry:map.entrySet()){
             int num = entry.getKey();
             int freq = entry.getValue();
