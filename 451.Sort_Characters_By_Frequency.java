46c5913ec2349896e041577ccb26b68ee7836992
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-04 04:27:21 +0530

    Solved

--- 451.Sort_Characters_By_Frequency.java
+++ 451.Sort_Characters_By_Frequency.java
@@ -8,6 +8,8 @@ class Solution {
         for(char ch : s.toCharArray()){
             map.put(ch, map.getOrDefault(ch, 0)+1);
         }
+
+        // store all unique char in list
         List<Character> list = new ArrayList<>(map.keySet());
         list.sort((a,b) -> map.get(b) - map.get(a));
         String ans = "";
