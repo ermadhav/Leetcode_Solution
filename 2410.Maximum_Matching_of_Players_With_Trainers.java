77f3222b2e2b3dd6908a54d8d3906db28aec942b
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-03 23:46:49 +0530

    Solved

--- 2410.Maximum_Matching_of_Players_With_Trainers.java
+++ 2410.Maximum_Matching_of_Players_With_Trainers.java
@@ -1,8 +1,10 @@
 class Solution {
     public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+
+        Arrays.sort(players);
+        Arrays.sort(trainers);
         int i=0,j =0; 
         int count =0;
-
         // running the loop into players and trainers 
         while(i<players.length && j< trainers.length){
             if(players[i] <= trainers[j]){
