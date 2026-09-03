2408aa7c7fe672532fc1044b61a9f77031ca3158
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-04 04:58:25 +0530

    Solved

--- 387.First_Unique_Character_in_a_String.java
+++ 387.First_Unique_Character_in_a_String.java
@@ -2,10 +2,12 @@ class Solution {
     public int firstUniqChar(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
 
-        // used to count the char and its freq
+        // used to count the freq
         for( char ch: s.toCharArray()){
             map.put(ch, map.getOrDefault(ch, 0)+1);
         }
+
+        // traverse the string to find 1st char with freq 1
         for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
             if(map.get(ch) == 1){
