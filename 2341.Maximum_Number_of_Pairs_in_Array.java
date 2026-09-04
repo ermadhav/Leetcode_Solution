e2bd962d3c789568b6af93ce67639e60751263ec
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-04 16:03:10 +0530

    Solved

--- 2341.Maximum_Number_of_Pairs_in_Array.java
+++ 2341.Maximum_Number_of_Pairs_in_Array.java
@@ -1,16 +1,22 @@
 // There are two approaches to solve this 
 
+
+// Approach --> 1
 class Solution {
     public int[] numberOfPairs(int[] nums) {
         int equalCount =0;
 
+        // constructing the arraylist because remove and adding is easy
         ArrayList<Integer> list = new ArrayList<>();
         for(int n:nums){
             list.add(n);
         }
 
+        // 
         for(int i=0; i<list.size(); i++){
             for(int j=i+1; j<list.size(); j++){
+
+                // If two elements are equal, we found a pair
                 if(list.get(i).equals(list.get(j))){
                     equalCount++;
                     list.remove(j);
@@ -28,5 +34,7 @@ class Solution {
 
     }
 }
+
+// Approach --> 2
 
 
