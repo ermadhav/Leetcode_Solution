949507533adc96dc5d99487777e7a352274bdbfe
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-07 22:15:54 +0530

    Solved

--- 3992.Rearrange_String_to_Avoid_Character_Pair.java
+++ 3992.Rearrange_String_to_Avoid_Character_Pair.java
@@ -1,14 +1,18 @@
 class Solution {
     public String rearrangeString(String s, char x, char y) {
         StringBuilder ans = new StringBuilder();
+
+        // first add all occurrences of y
         for(char c: s.toCharArray()){
             if(c==y){
                 ans.append(c);
             }
         }
+        // then add all occurence except x and y
         for(char c: s.toCharArray()){
             if(c != x && c != y) ans.append(c);
         }
+        // first add all occurrences of x
         for(char c: s.toCharArray()){
             if(c==x){
                 ans.append(c);
