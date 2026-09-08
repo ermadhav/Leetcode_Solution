c46d0e9e75ae72aacfb7adbc6f46ce1a0aa6a949
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-08 21:16:01 +0530

    Solved

--- 3870.Count_Commas_in_Range.java
+++ 3870.Count_Commas_in_Range.java
@@ -1,8 +1,12 @@
 class Solution {
     public int countCommas(int n) {
         int ans = 0;
+
+        // no comma under 999
         if(n <= 999){
             ans = 0;
+
+        // 1000 se n tak me har me 1 comma hota hai
         }else{
             ans = n-999;
         }
