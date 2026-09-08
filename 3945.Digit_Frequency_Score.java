4be9484ecf117e0a95be27220a14bc30ad5d893c
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-09 01:38:48 +0530

    Solved

--- 3945.Digit_Frequency_Score.java
+++ 3945.Digit_Frequency_Score.java
@@ -3,8 +3,12 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         // Number ke har digit ko check karo
         while(n>0){
+
+            // last digit nikalo
             int digit = n%10;
+            // digit ki freq incrs karo
             map.put(digit, map.getOrDefault(digit, 0) + 1);
+            // last digit remove karo
             n /= 10;
         }
         int ans = 0;
