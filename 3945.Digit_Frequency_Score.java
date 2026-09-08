4a8cad5d859bf51b9fd51895eb8e6afa4a6529e3
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-09 01:34:25 +0530

    Solved

--- 3945.Digit_Frequency_Score.java
+++ 3945.Digit_Frequency_Score.java
@@ -1,6 +1,7 @@
 class Solution {
     public int digitFrequencyScore(int n) {
         HashMap<Integer, Integer> map = new HashMap<>();
+        // Number ke har digit ko check karo
         while(n>0){
             int digit = n%10;
             map.put(digit, map.getOrDefault(digit, 0) + 1);
