c73c54b7719ebfade6dde678bfab4a665944192d
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-08 22:03:55 +0530

    Solved

--- 3028.Ant_on_the_Boundary.java
+++ 3028.Ant_on_the_Boundary.java
@@ -1,7 +1,7 @@
 class Solution {
     public int returnToBoundaryCount(int[] nums) {
-        int count = 0;  
-        int position = 0;
+        int count = 0;  // Boundary par kitni baar wapas aaya
+        int position = 0; // Ant ki current position
         for(int i=0; i<nums.length; i++){
             position += nums[i];
             if(position == 0){
