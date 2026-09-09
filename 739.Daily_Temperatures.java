d9abaccfbf81aceb66b1c00c9ed71f2a7b88e5ea
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-10 00:12:53 +0530

    Solved

--- 739.Daily_Temperatures.java
+++ 739.Daily_Temperatures.java
@@ -1,13 +1,14 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int[] ans = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+
         for(int i=0; i<temperatures.length; i++){
-            for(int j=i+1; j<temperatures.length; j++){
-                if(temperatures[j] > temperatures[i]){
-                    ans[i] = j-i;
-                    break;
-                }
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
+                int prev = stack.pop();
+                ans[prev] = i-prev;
             }
+            stack.push(i);
         }
         return ans;
     }
