665d7555b99b59fe121f2ad8f1c08e5473479cc5
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-10 00:22:56 +0530

    Solved

--- 739.Daily_Temperatures.java
+++ 739.Daily_Temperatures.java
@@ -14,6 +14,7 @@ class Solution {
                 // Number of days until warmer temperature
                 ans[prev] = i-prev;
             }
+            // Store current day's index
             stack.push(i);
         }
         return ans;
