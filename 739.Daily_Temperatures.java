786ed4e572d5853d50a5d17c27c6275229773e8d
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-10 00:17:09 +0530

    Solved

--- 739.Daily_Temperatures.java
+++ 739.Daily_Temperatures.java
@@ -1,9 +1,12 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        int[] ans = new int[temperatures.length];
-        Stack<Integer> stack = new Stack<>();
 
+        // stores the ans for each day
+        int[] ans = new int[temperatures.length];
+        // stack stores the indexes od days
+        Stack<Integer> stack = new Stack<>(); 
         for(int i=0; i<temperatures.length; i++){
+            // find the next warmer day 
             while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
                 int prev = stack.pop();
                 ans[prev] = i-prev;
