9ba0e80530cb1339eaba394767fc2991c116a637
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-11 02:03:40 +0530

    Solved

---
+++ 538.Convert_BST_to_Greater_Tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    int sum = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if(root == null) return null;
+
+        // traverse right first (greater elemeents)
+        bstToGst(root.right);
+
+        // Add current node to sum
+        sum += root.val;
+
+        // update curr node with greater tree value
+        root.val = sum;
+
+        // traverse right first (smaller elemeents)
+        bstToGst(root.left);
+        return root;
+    }
+}
