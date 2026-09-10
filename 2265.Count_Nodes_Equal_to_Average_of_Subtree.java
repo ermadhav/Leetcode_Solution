fbf059a760172a85e7db88aede2f004acec3bf12
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-11 00:11:18 +0530

    Solved

--- 2265.Count_Nodes_Equal_to_Average_of_Subtree.java
+++ 2265.Count_Nodes_Equal_to_Average_of_Subtree.java
@@ -1,25 +1,25 @@
-class Solution {
-    int result = 0;
-    public int averageOfSubtree(TreeNode root) {
-        dfs(root);
-        return result;
-    }
-    public int[] dfs(TreeNode node){
-        if (node == null) {
-            return new int[]{0, 0};
-        }
+// class Solution {
+//     int result = 0;
+//     public int averageOfSubtree(TreeNode root) {
+//         dfs(root);
+//         return result;
+//     }
+//     public int[] dfs(TreeNode node){
+//         if (node == null) {
+//             return new int[]{0, 0};
+//         }
 
-        int[] left = dfs(node.left);
-        int[] right = dfs(node.right);
+//         int[] left = dfs(node.left);
+//         int[] right = dfs(node.right);
 
-        int totalSum = left[0] + right[0] + node.val;
-        int totalCount = left[1] + right[1] + 1;
+//         int totalSum = left[0] + right[0] + node.val;
+//         int totalCount = left[1] + right[1] + 1;
 
-        if(totalSum/ totalCount == node.val){
-            result++;
-        }
+//         if(totalSum/ totalCount == node.val){
+//             result++;
+//         }
 
-        return new int[]{totalSum, totalCount};
+//         return new int[]{totalSum, totalCount};
 
-    }
-}
+//     }
+// }
