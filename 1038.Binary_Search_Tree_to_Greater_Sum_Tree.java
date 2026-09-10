e02fa0dcf1bb2321fd50e03171098a68e6e7425e
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-11 01:42:25 +0530

    Solved

---
+++ 1038.Binary_Search_Tree_to_Greater_Sum_Tree.java
@@ -0,0 +1,13 @@
+class Solution {
+    int sum = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if(root == null) return null;
+
+        // traverse right first (greater elemeents)
+        bstToGst(root.right);
+        sum += root.val;
+        root.val = sum;
+        bstToGst(root.left);
+        return root;
+    }
+}
