ede68b4f986b84d38849a2daeaccaf4d9a9b1366
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-14 01:07:29 +0530

    Solved

--- 835.Image_Overlap.java
+++ 835.Image_Overlap.java
@@ -8,8 +8,12 @@ class Solution {
             //try every vertical shift
             for(int right=-(n-1); right<=n-1; right++ ){
                 int count =0;
+
+                // checking every cell of imgage 1
                 for(int row=0; row<n; row++){
                     for(int col=0; col<n; col++){
+
+                        // only move cell which has 1
                         if(img1[row][col] == 1){
                             int newRow= row+down;
                             int newCol= col+right;
