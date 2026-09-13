54a63a4895f2c1ea3da04da1c51c956af5924b84
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-13 17:15:00 +0530

    Solved

--- 3857.Minimum_Cost_to_Split_into_Ones.java
+++ 3857.Minimum_Cost_to_Split_into_Ones.java
@@ -1,8 +1,13 @@
 class Solution {
     public int minCost(int n) {
+        // stores the min cost 
         int[] dp = new int[n+1];
         dp[1] = 0;
+
+        // find ans from 2 to n
         for(int x = 2; x <=n; x++){
+
+            // setting a very large no. initially
             dp[x] = Integer.MAX_VALUE;
             for(int a =1; a<x; a++){
                 int b = x-a;
