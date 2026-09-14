6e96e8e185c043139dcf1bce4480f457a9df7060
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-14 23:47:06 +0530

    Solved

--- 3750.Minimum_Number_of_Flips_to_Reverse_Binary_String.java
+++ 3750.Minimum_Number_of_Flips_to_Reverse_Binary_String.java
@@ -1,10 +1,13 @@
 class Solution {
     public int minimumFlips(int n) {
+    
+        // converting int into binary
     String s=Integer.toBinaryString(n);
     int i=0;
     int j=s.length()-1;
     int count =0;
 
+    // comparing bits from front and bakc
     while(i<j){
         if(s.charAt(i) != s.charAt(j)){
             count +=2;
