18499364ac4603422c6e452201effdf426fcea08
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-15 22:31:55 +0530

    Solved

--- 1941.Check_if_All_Characters_Have_Equal_Number_of_Occurrences.java
+++ 1941.Check_if_All_Characters_Have_Equal_Number_of_Occurrences.java
@@ -7,6 +7,8 @@ class Solution {
             map.put(ch, map.getOrDefault(ch,0)+1);
         }
         int freq =-1;
+
+        // checking if all char has same freq
         for(int f : map.values()){
             if(freq == -1){
                 freq = f;
