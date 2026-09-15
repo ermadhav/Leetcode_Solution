5eb29c4fca975bb4a814c444db026b5d4da5d2e4
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-15 22:29:18 +0530

    Solved

--- 1941.Check_if_All_Characters_Have_Equal_Number_of_Occurrences.java
+++ 1941.Check_if_All_Characters_Have_Equal_Number_of_Occurrences.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean areOccurrencesEqual(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
+
+        // counting freq of each char
         for(char ch : s.toCharArray()){
             map.put(ch, map.getOrDefault(ch,0)+1);
         }
