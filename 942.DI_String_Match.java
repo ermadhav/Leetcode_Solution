dedbf48d26ee50d859414844dc7a23f9f6e2b36e
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-16 22:01:41 +0530

    Solved

--- 942.DI_String_Match.java
+++ 942.DI_String_Match.java
@@ -1,18 +1,27 @@
 class Solution {
     public int[] diStringMatch(String s) {
         int n = s.length();
+
+        // smallest avil no.
         int low = 0;
+        // largest avail no.
         int high = n;
+        // store the perm
         ArrayList<Integer> list = new ArrayList<>();
+        // travering the each char of string
         for(char c : s.toCharArray()){
+
+            // we need an increasing value so add the smallest available number
             if(c == 'I'){
                 list.add(low);
                 low++;
+            // we need a decreasing value so add the largest available number
             }else{
                 list.add(high);
                 high--;
             }
         }
+        // adding the last remaning no.
         list.add(low);
         int[] ans = new int[list.size()];
         for(int i=0; i<list.size(); i++){
