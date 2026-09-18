c78bedd2b2a347e3125e71919d4e5870737a1b5b
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-18 17:07:02 +0530

    Solved

--- 3931.Check_Adjacent_Digit_Differences.java
+++ 3931.Check_Adjacent_Digit_Differences.java
@@ -1,17 +1,15 @@
 class Solution {
     public boolean isAdjacentDiffAtMostTwo(String s) {
         int[] arr = new int[s.length()];
-        boolean ans = true;
+        // converting no. string to int arr
         for(int i=0; i<s.length(); i++){
             arr[i] = s.charAt(i)-'0';
         }
         for(int i=0; i<s.length()-1; i++){
-            if(Math.abs(arr[i]-arr[i+1])>=2){
-                ans = false;
-            }else{
-                ans = true;
+            if(Math.abs(arr[i]-arr[i+1])>2){
+                return false;
             }
         }
-        return ans;
+        return true;
     }
 }
