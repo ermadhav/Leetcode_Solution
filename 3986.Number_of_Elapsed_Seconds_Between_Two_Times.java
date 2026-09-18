fbc5bfb96310248b207868edd20de65e885d9c50
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-18 18:27:06 +0530

    Solved

--- 3986.Number_of_Elapsed_Seconds_Between_Two_Times.java
+++ 3986.Number_of_Elapsed_Seconds_Between_Two_Times.java
@@ -5,9 +5,10 @@ class Solution {
         String[] partOne = startTime.split(":");
         String[] partTwo = endTime.split(":");
 
-
+        // total seconds in One(StartTime)
         int totalSecondOne = 3600*Integer.parseInt(partOne[0])+60*Integer.parseInt(partOne[1])+Integer.parseInt(partOne[2]);
 
+        // total seconds in Two(TwoTime)
         int totalSecondTwo = 3600*Integer.parseInt(partTwo[0])+60*Integer.parseInt(partTwo[1])+Integer.parseInt(partTwo[2]);
 
         return totalSecondTwo-totalSecondOne;
