8d6380e3fa0d653137299e2e5e345b39d18110fd
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-18 17:08:21 +0530

    Solved

--- 3931.Check_Adjacent_Digit_Differences.java
+++ 3931.Check_Adjacent_Digit_Differences.java
@@ -5,11 +5,15 @@ class Solution {
         for(int i=0; i<s.length(); i++){
             arr[i] = s.charAt(i)-'0';
         }
+
+        // calcutlating abs diff 
         for(int i=0; i<s.length()-1; i++){
+            // if > 2 return false
             if(Math.abs(arr[i]-arr[i+1])>2){
                 return false;
             }
         }
+        // otherwise true
         return true;
     }
 }
