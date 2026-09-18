8520fa06fcac8b5638c8ee55248f1c45044b93ee
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-19 01:03:38 +0530

    Solved

--- 2710.Remove_Trailing_Zeros_From_a_String.java
+++ 2710.Remove_Trailing_Zeros_From_a_String.java
@@ -1,9 +1,12 @@
 class Solution {
     public String removeTrailingZeros(String num) {
         int i = num.length()-1;
+
+        // finding from last if found just decrease the index till non zero
         while(num.charAt(i) == '0'){
             i--;
         }
+        // then return the substring
         return num.substring(0, i+1);
     }
 }
