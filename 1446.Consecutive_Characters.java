1e83d57cef3b04add492e289cf9b1e9b13f1eef5
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-20 00:43:49 +0530

    Solved

--- 1446.Consecutive_Characters.java
+++ 1446.Consecutive_Characters.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxPower(String s) {
-        int curr =1; // curr = 1 bec    ause the strings are non-empty
+        int curr =1; 
         int max =1; // 
         for(int i=0; i<s.length()-1; i++){
             if(s.charAt(i) == s.charAt(i+1)){
@@ -8,7 +8,7 @@ class Solution {
             }else{
                 curr = 1; // if not then reset
             }
-            
+            // the longest found till now
             max = Math.max(curr, max);
         }
         return max;
