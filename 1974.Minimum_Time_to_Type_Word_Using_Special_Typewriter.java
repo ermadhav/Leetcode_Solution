8b59bf0d1a81dbf41408371166fbbccef666ab8f
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-19 12:14:07 +0530

    Solved

--- 1974.Minimum_Time_to_Type_Word_Using_Special_Typewriter.java
+++ 1974.Minimum_Time_to_Type_Word_Using_Special_Typewriter.java
@@ -6,7 +6,7 @@ class Solution {
         // typewritter starts from 'a'
         int curr = 0;
         for(int i=0; i<l; i++){
-            // converting char into num
+            // converting target char into num
             int target = word.charAt(i) - 'a';
             int cw = Math.abs(curr-target);
             int acw = 26-cw;
