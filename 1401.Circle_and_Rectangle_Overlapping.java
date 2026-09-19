a878fc537e793c7920f43366ccaca570aa8b2b86
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-19 21:51:26 +0530

    Solved

---
+++ 1401.Circle_and_Rectangle_Overlapping.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        int closestX = Math.max(x1, Math.min(x2, xCenter));
+        int closestY = Math.max(y1, Math.min(y2, yCenter));
+
+        int diffX = xCenter - closestX;
+        int diffY = yCenter - closestY;
+        
+        if(diffX*diffX+diffY*diffY <= radius) return true;
+
+        return false;
+    }
+}
