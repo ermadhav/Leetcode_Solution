42964c1abd7fcbd8538188debe79e80b0c319ea1
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-19 21:56:11 +0530

    Solved

--- 1401.Circle_and_Rectangle_Overlapping.java
+++ 1401.Circle_and_Rectangle_Overlapping.java
@@ -4,6 +4,7 @@ class Solution {
         int closestX = Math.max(x1, Math.min(x2, xCenter));
         int closestY = Math.max(y1, Math.min(y2, yCenter));
 
+        diff between center and closestX and Y
         int diffX = xCenter - closestX;
         int diffY = yCenter - closestY;
 
