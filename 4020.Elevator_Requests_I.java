abf3ee384294550ba60acfa3b0549c131b7f4415
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-21 00:08:20 +0530

    Solved

--- 4020.Elevator_Requests_I.java
+++ 4020.Elevator_Requests_I.java
@@ -1,5 +1,7 @@
 class Solution {
     public int elevatorRequests(int n, int[] requests) {
+
+        // dist from 1st floor to first req
         int ans =requests[0];
         for(int i=1; i<requests.length; i++){
             ans += Math.abs(requests[i]-requests[i-1]);
