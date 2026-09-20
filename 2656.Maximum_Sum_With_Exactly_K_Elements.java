230f7ac93609df1740aade3279cc92578f4825aa
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-21 02:40:24 +0530

    Solvled

--- 2656.Maximum_Sum_With_Exactly_K_Elements.java
+++ 2656.Maximum_Sum_With_Exactly_K_Elements.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maximizeSum(int[] nums, int k) {
+        // sorting beacuse we need the last largest no.
         Arrays.sort(nums);
         int l = nums.length-1;
         int ans = nums[l]*k+ (k*(k-1)/2);
