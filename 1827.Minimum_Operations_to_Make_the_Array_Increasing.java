eb1f89f1e63f6d383dbb3b647ec8a94de4a7b11e
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-21 00:36:30 +0530

    Solved

---
+++ 1827.Minimum_Operations_to_Make_the_Array_Increasing.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int ans = 0;
+        if(nums.length == 1) return 0;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] <= nums[i-1]){
+                int val=nums[i-1]+1;
+                ans +=val-nums[i];
+                nums[i] = val;
+            }
+        }
+        return ans;
+    }
+}
