06c1ec1a230f9bd97d18d8678226825dbadad033
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-21 01:40:32 +0530

    Solved

---
+++ 3917.Count_Indices_With_Opposite_Parity.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] countOppositeParity(int[] nums){
+        int[] ans = new int[nums.length];
+        int n = nums.length;
+        int odd =0;
+        int even=0;
+        // traverse from right to left
+        for(int i=n-1; i>=0; i--){
+            if(nums[i]%2==0){
+                ans[i] = odd;
+                even++;
+            }else{
+                ans[i] = even;
+                odd++;
+            }
+        }
+        return ans[];
+    }
+}
