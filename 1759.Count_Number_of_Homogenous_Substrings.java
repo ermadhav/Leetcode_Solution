f4d2e38ed8795b1f7358fc0e8f3f43cbba130b1a
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-20 18:15:19 +0530

    Solved

--- 1759.Count_Number_of_Homogenous_Substrings.java
+++ 1759.Count_Number_of_Homogenous_Substrings.java
@@ -4,6 +4,7 @@ class Solution {
         long count =0;
         int mod =1000000007;
         for(int i=0; i<s.length(); i++){
+            // if same char incrs count
             if(i>0 && s.charAt(i) == s.charAt(i-1)){
                 count++;
             }else{
