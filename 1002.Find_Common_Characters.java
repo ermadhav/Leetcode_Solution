ab4bfa6bba91c17cffff96fbc16d7900576ea1ee
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-21 20:12:46 +0530

    Solved

---
+++ 1002.Find_Common_Characters.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> commonChars(String[] words) {
+        int[] freq =new int[26];
+        for(char ch: words[0].toCharArray()){
+            freq[ch - 'a']++;
+        }
+        for(int i=1; i<words.length; i++){
+            int[] temp=new int[26]; 
+            for(char ch: words[i].toCharArray()){
+                temp[ch - 'a']++;
+            }
+            for(int j=0; j<26; j++){
+                freq[j]=Math.min(freq[j], temp[j]);
+            }
+        }
+        List<String> ans=new ArrayList<>();
+        for(int i=0; i<26; i++){
+            while(freq[i] --> 0){
+                ans.add(""+(char)(i+'a'));
+            }
+        }
+        return ans;
+    }
+}
