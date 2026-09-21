ee1d7d7c0a934826d35eb645be54544c5eda6480
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-21 19:48:26 +0530

    Solved

--- 2215.Find_the_Difference_of_Two_Arrays.java
+++ 2215.Find_the_Difference_of_Two_Arrays.java
@@ -1,5 +1,7 @@
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+
+        // adding in set to remove duplicates
         Set<Integer> set1 =new HashSet<>();
         Set<Integer> set2 =new HashSet<>();
 
@@ -9,11 +11,14 @@ class Solution {
         List<Integer> x =new ArrayList<>();
         List<Integer> y =new ArrayList<>();
 
+        // elemtn in num1 but not in num2
         for(int n:set1){
             if(!set2.contains(n)){
                 x.add(n);
             }
         }
+        
+        // elemtn in num1 but not in num2
         for(int n:set2){
             if(!set1.contains(n)){
                 y.add(n);
