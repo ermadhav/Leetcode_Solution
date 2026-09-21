b7afbf2bc90bb86b4af80f716f6a5b80c1e62050
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-22 00:21:48 +0530

    Solved

---
+++ 3823.Reverse_Letters_Then_Special_Characters_in_a_String.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String reverseByType(String s) {
+        List<Character> letter = new ArrayList<>();
+        List<Character> special = new ArrayList<>();
+
+        // separing letter and special
+        for(char ch : s.toCharArray()){
+            if(Character.isLowerCase(ch)){
+                letter.add(ch);
+            }else{
+                special.add(ch);
+            }
+        }
+        Collections.reverse(letter);
+        Collections.reverse(special);
+
+        StringBuilder ans = new StringBuilder();
+        int l=0;
+        int spcl =0;
+        for(char ch : s.toCharArray()){
+            if(Character.isLowerCase(ch)){
+                ans.append(letter.get(l));
+                l++;
+            }else{
+                ans.append(special.get(spcl));
+                spcl++;
+            }
+        }
+        return ans.toString();
+    }
+}
