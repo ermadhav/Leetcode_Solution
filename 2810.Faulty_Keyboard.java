f35e130f7acfe3a5dde6689da8dc8bad05aa2ffc
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-22 01:26:25 +0530

    Solved

--- 2810.Faulty_Keyboard.java
+++ 2810.Faulty_Keyboard.java
@@ -19,3 +19,19 @@ class Solution {
         return sb.toString();
     }
 }
+
+// Approach --- 2 
+
+class Solution {
+    public String finalString(String s) {
+        StringBuilder ans = new StringBuilder();
+        for(char ch:s.toCharArray())  {
+            if(ch != 'i'){
+                ans.append(ch);
+            }else{
+                ans.reverse();
+            }
+        }
+        return ans.toString();
+    }
+}
