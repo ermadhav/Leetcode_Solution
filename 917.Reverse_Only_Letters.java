08f62bd5b18252e2ec5facce411821e582e5ca10
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-22 00:57:04 +0530

    Solved

--- 917.Reverse_Only_Letters.java
+++ 917.Reverse_Only_Letters.java
@@ -24,6 +24,7 @@ class Solution {
 
         // using this for og postion
         for(char ch : s.toCharArray()){
+            // if char is letter then put the reversed 
             if(Character.isLetter(ch)){
                 ans.append(letter.get(l));
                 l++;
