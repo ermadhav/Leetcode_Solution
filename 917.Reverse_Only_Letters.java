2603a9d303b111b08a97565ed0297e7db0d1f27f
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-22 00:45:55 +0530

    Solved

---
+++ 917.Reverse_Only_Letters.java
@@ -0,0 +1,34 @@
+// Same Logic Behind 
+
+class Solution {
+    public String reverseOnlyLetters(String s) {
+
+        // store letter and char separately
+        List<Character> letter = new ArrayList<>();
+        List<Character> special = new ArrayList<>();
+
+        for(char ch : s.toCharArray()){
+            if(Character.isLetter(ch)){
+                letter.add(ch);
+            }else{
+                special.add(ch);
+            }
+        }
+
+        Collections.reverse(letter);
+
+        StringBuilder ans = new StringBuilder();
+        int l=0;
+        int spcl =0;
+        for(char ch : s.toCharArray()){
+            if(Character.isLetter(ch)){
+                ans.append(letter.get(l));
+                l++;
+            }else{
+                ans.append(special.get(spcl));
+                spcl++;
+            }
+        }
+        return ans.toString();
+    }
+}
