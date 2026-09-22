5b43fa9c776354c450b902f4658c923b0a697e64
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-22 23:56:36 +0530

    Solved

--- 3019.Number_of_Changing_Keys.java
+++ 3019.Number_of_Changing_Keys.java
@@ -2,6 +2,8 @@ class Solution {
     public int countKeyChanges(String s) {
         int count =0;
         for(int i=0; i<s.length()-1; i++){
+
+            // converting both char before comparing
             if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(i+1))){
                 count++;
             }
