68bd47243ce4d9ed7504c2685d2ab6336a501087
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-23 03:02:32 +0530

    Solved

---
+++ 4006.Count_Valid_Prefixes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countValidPrefixes(String s) {
+        int ones =0;
+        int zeros =0;
+        int ans=0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '0'){
+                zeros++;
+            }else{
+                ones++;
+            }
+            if(Math.abs(zeros-ones) <= 1){
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+}
