7af30800908b8cbb7f40f00fba35c18560cd4a89
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-23 03:27:32 +0530

    Solved

--- 3324.Find_the_Sequence_of_Strings_Appeared_on_the_Screen.java
+++ 3324.Find_the_Sequence_of_Strings_Appeared_on_the_Screen.java
@@ -3,9 +3,11 @@ class Solution {
         List<String> ans =new ArrayList<>();
         StringBuilder s =new StringBuilder();
         for(char ch :target.toCharArray()){
+            // key1: append 'a'
             s.append('a');
             ans.add(s.toString());
 
+             // key2: change 'a' to the required char
             for(char c='b'; c<= ch; c++){
                 s.setCharAt(s.length()-1,c);
                 ans.add(s.toString());
