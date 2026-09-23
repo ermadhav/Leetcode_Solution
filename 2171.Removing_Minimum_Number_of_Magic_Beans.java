e880b019189f5df69c5b7a19e4e8a7eff9efb7b4
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-23 23:30:24 +0530

    Solved

--- 2171.Removing_Minimum_Number_of_Magic_Beans.java
+++ 2171.Removing_Minimum_Number_of_Magic_Beans.java
@@ -3,11 +3,9 @@ class Solution {
         Arrays.sort(beans);
         long total =0;
         long ans = Long.MAX_VALUE;
-        // Calculate total beans
         for(int i=0; i<beans.length; i++){
             total += beans[i];
         }
-        // remove all beans from bags before i, and reduce remaining bags to beans[i]
         long prefix=0;
         for(int i=0; i<beans.length; i++){
             long remove = prefix+(total - (long) beans[i]*(beans.length-i));
