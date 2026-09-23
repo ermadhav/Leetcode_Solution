01310f3f0808a2f5214ed78da5a83c2a387de59c
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-23 23:14:24 +0530

    Solved

---
+++ 2171.Removing_Minimum_Number_of_Magic_Beans.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long minimumRemoval(int[] beans) {
+        Arrays.sort(beans);
+        int total =0;
+        long ans = Long.MAX_VALUE;
+        for(int i=0; i<beans.length; i++){
+            total += beans[i];
+        }
+        long prefix=0;
+        for(int i=0; i<beans.length; i++){
+            long remove = prefix+(total - (long) beans[i]*(beans.length-i));
+            ans = Math.min(ans, remove);
+        }
+        return ans; 
+    }
+}
