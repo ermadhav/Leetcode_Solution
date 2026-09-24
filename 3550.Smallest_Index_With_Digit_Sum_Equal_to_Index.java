b6bb7a9d5ed4eff955d6b03fba4ae5f04f6ad039
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-24 15:59:19 +0530

    Solved

--- 3550.Smallest_Index_With_Digit_Sum_Equal_to_Index.java
+++ 3550.Smallest_Index_With_Digit_Sum_Equal_to_Index.java
@@ -1,20 +1,41 @@
+// Approach --> 1
+
+// class Solution {
+//     public int smallestIndex(int[] nums) {
+//         for (int i = 0; i < nums.length; i++) {
+//             int sum = digitSum(nums[i]);
+//             if (sum == i) {
+//                 return i; // Return first match (smallest index)
+//             }
+//         }
+//         return -1; // No match found
+//     }
+
+//     private int digitSum(int num) {
+//         int sum = 0;
+//         while (num > 0) {
+//             sum += num % 10; // Add last digit
+//             num /= 10;       // Remove last digit
+//         }
+//         return sum;
+//     }
+// }
+
+// Approach --> 2
+
 class Solution {
     public int smallestIndex(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            int sum = digitSum(nums[i]);
-            if (sum == i) {
-                return i; // Return first match (smallest index)
+        for(int i=0; i<nums.length; i++){
+            int temp = nums[i];
+            int sum = 0;
+            while(temp>0){
+                sum += temp % 10;
+                temp /= 10;
+            }
+            if(sum == i){
+                return i;
             }
         }
-        return -1; // No match found
-    }
-
-    private int digitSum(int num) {
-        int sum = 0;
-        while (num > 0) {
-            sum += num % 10; // Add last digit
-            num /= 10;       // Remove last digit
-        }
-        return sum;
+        return -1;
     }
 }
