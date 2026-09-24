a2d52d10b84c3091082343ae639166952fac3788
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-25 01:07:20 +0530

    Solved

--- 3745.Maximize_Expression_of_Three_Elements.java
+++ 3745.Maximize_Expression_of_Three_Elements.java
@@ -5,11 +5,12 @@ class Solution {
         for (int i=0;i<nums.length;i++) {
             for (int j=i+1;j<nums.length;j++) {
                 for (int k=j+1;k<nums.length;k++) {
+                    // 
                     int a=0, b=0, c=0;
                     a = nums[i];
                     b = nums[j];
                     c = nums[k];
-                    
+                    // trying each element for ans
                     ans = Math.max(ans, a+b-c);
                     ans = Math.max(ans, b+c-a);
                     ans = Math.max(ans, c+a-b);
