7904a44b9e3b24bc2a8664349febf02f12fe0fd5
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-09-25 11:46:50 +0530

    Solved

--- 3996.Even_Number_of_Knight_Moves.java
+++ 3996.Even_Number_of_Knight_Moves.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean canReach(int[] start, int[] target) {
+        // checking if both cells have the same color
         return (start[0]+start[1])%2 == (target[0]+target[1])%2;
     }
 }
